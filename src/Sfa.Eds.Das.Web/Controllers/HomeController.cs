93be8edec47d3e21a4e977956eacefee9ac1638f
Lee Wadhams <lee.wadhams@valtech.com>
2015-12-15 12:14:16 +0000

    Added stub search results.

--- src/Sfa.Eds.Das.Web/Controllers/HomeController.cs
+++ src/Sfa.Eds.Das.Web/Controllers/HomeController.cs
@@ -1,8 +1,6 @@
-﻿using System;
+﻿using System.Web.Mvc;
+using Sfa.Eds.Das.Web.Models;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
 
 namespace Sfa.Eds.Das.Web.Controllers
 {
@@ -13,14 +11,43 @@ namespace Sfa.Eds.Das.Web.Controllers
             return View();
         }
 
+        [HttpPost]
         public ActionResult Search(SearchCriteria criteria)
         {
-            return View();
-        }
-    }
+            var searchResults = CreateStubSearchResults();
 
-    public class SearchCriteria
-    {
-        public string Keywords { get; set; }
+            return View(searchResults);
+        }
+
+        private static SearchResults CreateStubSearchResults()
+        {
+            var results = new SearchResults
+            {
+                TotalResults = 5,
+                Results = CreateListOfResults()
+            };
+
+            return results;
+        }
+
+        private static IEnumerable<SearchResultsItem> CreateListOfResults()
+        {
+            var resultItems = new List<SearchResultsItem>(5)
+            {
+                new SearchResultsItem {Name = "DENTAL_HEALTH_Dental_Technician.ashx.pdf", TextSnippet = CreateSnippetText()},
+                new SearchResultsItem {Name = "BUTCHERY_Butcher.ashx.pdf", TextSnippet = CreateSnippetText()},
+                new SearchResultsItem {Name = "AUTOMOTIVE_Manufacturing_Engineer_standard.ashx.pdf", TextSnippet = CreateSnippetText()},
+                new SearchResultsItem {Name = "ACTUARIAL_-_Actuarial_Technician.pdf", TextSnippet = CreateSnippetText()},
+                new SearchResultsItem {Name = "AEROSPACE_-_Aerospace_Engineer.pdf", TextSnippet = CreateSnippetText()}
+            };
+
+            return resultItems;
+        }
+
+        private static string CreateSnippetText()
+        {
+            return
+                "Bacon ipsum dolor amet incididunt flank dolore beef chicken ham hock prosciutto ad sunt et fugiat aute dolore pork belly laborum. Flank in ut aliquip ground round eu. Pastrami ball tip sirloin spare ribs ut sed aliquip est turkey beef ribs drumstick boudin venison. Short ribs minim bacon labore veniam ea jerky kevin picanha turkey brisket adipisicing tri-tip.";
+        }
     }
 }
---
+++ src/Sfa.Eds.Das.Web/Models/SearchResultsItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Sfa.Eds.Das.Web.Models
+{
+    public sealed class SearchResultsItem
+    {
+        public string Name { get; set; }
+
+        public string TextSnippet { get; set; }
+    }
+}
