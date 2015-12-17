df483bb7c906a2e0c83f86bb6add7aaf90e4ba3b
nasirkhan <nasiralikhan1982@gmail.com>
2015-12-17 16:44:20 +0000

    Automation Acceptance tests

---
+++ webtests/Sfa.Eds.Das.Web.AcceptanceTests/Pages/BasePage.cs
@@ -0,0 +1,114 @@
+﻿using OpenQA.Selenium;
+using System;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium.Chrome;
+using System.Configuration;
+
+namespace Sfa.Eds.Das.Web.AcceptanceTests.Pages
+{
+    class BasePage // :  Base
+    {
+        public IWebDriver driver;
+        private static string baseUrl;
+
+        //public object ConfigurationManager { get; private set; }
+
+        public BasePage()
+        {
+
+            
+            //driver = (IWebDriver)ScenarioContext.Current["driver"];
+            //baseUrl = ConfigurationManager.AppSettings["baseUrl"];
+
+
+
+
+
+            // driver = (IWebDriver)ScenarioContext.Current["driver"];
+            // driver = new ChromeDriver(@"C:\\Users\\khann\\Documents\\Visual Studio 2015\\Projects\\DAS_WebTests\\DAS_WebTests\\Test\Resources");
+
+            // baseUrl = ConfigurationManager.AppSettings["baseUrl"];
+        }
+
+
+        public void StartSelenium()
+        {
+            try
+            {
+                driver = new ChromeDriver(@"C:\\Users\\khann\\Documents\\Visual Studio 2015\\Projects\\DAS_WebTests\\Sfa.Eds.Das.Web.AcceptanceTests\\Test\Resources");
+                // driver.FindElement(By.TagName("body")).SendKeys(Keys.F11);
+
+            }
+            catch
+            {
+
+            }
+
+
+        }
+
+        
+        public void type(string inputText, By locator)
+        {
+            find(locator).SendKeys(inputText);
+        }
+
+        public IWebElement find(By locator)
+        {
+            // WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
+            //  wait.Until(ExpectedConditions.ElementIsVisible(locator));
+            return driver.FindElement(locator);
+        }
+
+        public void verifyPage(string pageTitle)
+        {
+            if (!pageTitle.Equals(driver.Title))
+            {
+                throw new InvalidOperationException("This page is not " + pageTitle + ". The title is: " + driver.Title);
+            }
+        }
+
+
+        public void Launch(string url, string pageTitle)
+        {
+
+            driver.Navigate().GoToUrl(baseUrl + url);
+            verifyPage(pageTitle);
+        }
+
+       
+        public void click(By locator)
+        {
+            find(locator).Click();
+        }
+
+       
+        public string getText(By locator)
+        {
+            return find(locator).Text;
+        }
+
+
+
+        public bool isDisplayed(By locator)
+        {
+            try
+            {
+                IWebElement element = find(locator);
+                return element.Displayed && element.Enabled;
+            }
+            catch (NoSuchElementException ex)
+            {
+                return false;
+            }
+
+        }
+public void closeBrowser()
+        {
+            driver.Quit();
+
+
+        }
+
+    }
+}
---
+++ webtests/Sfa.Eds.Das.Web.AcceptanceTests/StepDefinitions/StandardsSearchSteps.cs
@@ -0,0 +1,119 @@
+﻿
+using Sfa.Eds.Das.Web.AcceptanceTests.Pages;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Text;
+using TechTalk.SpecFlow;
+
+
+namespace Sfa.Eds.Das.Web.AcceptanceTests.StepDefinitions
+{
+
+    
+   
+  [Binding]
+    public class StandardsSearchSteps
+    {
+
+        SearchPage srchPage;
+       
+
+         
+
+        [Given(@"I am on Search landing page")]
+        public void GivenIAmOnSearchLandingPage()
+        {
+
+            srchPage = new SearchPage();
+            srchPage.StartSelenium();
+            srchPage.launchLandingPage();
+            
+            
+            
+      
+
+           // ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I enter keyword '(.*)' in search box")]
+        public void GivenIEnterKeywordInSearchBox(string p0)
+        {
+            // srchPage.enterSearchBox(p0);
+            //  ScenarioContext.Current.Pending();
+
+            srchPage.SearchKeyword(p0);
+
+        }
+        
+        [When(@"I click on search button")]
+        public void WhenIClickOnSearchButton()
+        {
+            srchPage.clickSearchBox();
+        }
+
+
+        [Then(@"I should see matching '(.*)' standards on result page")]
+        public void ThenIShouldSeeMatchingStandardsOnResultPage(string p0)
+        {
+            srchPage.verifyPage("- Employer Apprenticeship Search");
+            srchPage.verifyresultsPages();
+            srchPage.verifyStandardFoundinResultPage(p0);
+           
+        }
+
+[AfterScenario]
+
+public void closeBrowser()
+        {
+            if (ScenarioContext.Current.TestError != null)
+            {
+                IWebDriver driver = null;
+                TakeScreenshot(driver);
+            }
+            srchPage.closeBrowser();
+
+
+        }
+        private static void TakeScreenshot(IWebDriver driver)
+        {
+            try
+            {
+                string fileNameBase = string.Format("error_{0}_{1}_{2}",
+                    FeatureContext.Current.FeatureInfo.Title,
+                    ScenarioContext.Current.ScenarioInfo.Title,
+                    DateTime.Now.ToString("yyyyMMdd_HHmmss"));
+
+                var artifactDirectory = Path.Combine(Directory.GetCurrentDirectory(), "testresults");
+                if (!Directory.Exists(artifactDirectory))
+                    Directory.CreateDirectory(artifactDirectory);
+
+                string pageSource = driver.PageSource;
+                string sourceFilePath = Path.Combine(artifactDirectory, fileNameBase + "_source.html");
+                File.WriteAllText(sourceFilePath, pageSource, Encoding.UTF8);
+                Console.WriteLine("Page source: {0}", new Uri(sourceFilePath));
+
+                ITakesScreenshot takesScreenshot = driver as ITakesScreenshot;
+
+                if (takesScreenshot != null)
+                {
+                    var screenshot = takesScreenshot.GetScreenshot();
+                    string screenshotFilePath = Path.Combine(artifactDirectory, fileNameBase + "_screenshot.png");
+                    screenshot.SaveAsFile(screenshotFilePath, ImageFormat.Png);
+                    Console.WriteLine("Screenshot: {0}", new Uri(screenshotFilePath));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error while taking screenshot: {0}", ex);
+            }
+        }
+    }
+
+
+
+
+}
+
