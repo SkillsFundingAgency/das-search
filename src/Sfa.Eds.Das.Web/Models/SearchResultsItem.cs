24f5962b8fa58c7e8a4412c658ae5580f2e99ce2
mapaux <josevi.agullo@gmail.com>
2015-12-18 11:15:31 +0000

    Added attributes to SearchResultItem and Search view

--- src/Sfa.Eds.Das.Web/Models/SearchResultsItem.cs
+++ src/Sfa.Eds.Das.Web/Models/SearchResultsItem.cs
@@ -1,7 +1,11 @@
-﻿namespace Sfa.Eds.Das.Web.Models
+﻿using System;
+
+namespace Sfa.Eds.Das.Web.Models
 {
     public sealed class SearchResultsItem
     {
         public string Title { get; set; }
+        public float FundingCap { get; set; }
+        public DateTime EffectiveEndDay { get; set; }
     }
 }
