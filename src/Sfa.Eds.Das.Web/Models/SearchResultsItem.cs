749293429deb9999d605f513d692029942c7d8df
mapaux <josevi.agullo@gmail.com>
2015-12-21 13:13:04 +0000

    Removed unnecessary elements at Search view

--- src/Sfa.Eds.Das.Web/Models/SearchResultsItem.cs
+++ src/Sfa.Eds.Das.Web/Models/SearchResultsItem.cs
@@ -7,7 +7,5 @@ namespace Sfa.Eds.Das.Web.Models
         public string Title { get; set; }
         public int NotionalEndLevel { get; set; }
         public string PdfUrl { get; set; }
-        public float FundingCap { get; set; }
-        public DateTime EffectiveEndDay { get; set; }
     }
 }
