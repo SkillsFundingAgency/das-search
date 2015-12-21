344b2300ac2d11202ad1425c38b598a6be8275e7
mapaux <josevi.agullo@gmail.com>
2015-12-21 12:11:37 +0000

    Updated website with new attributes from metadata

--- src/Sfa.Eds.Das.Web/Models/SearchResultsItem.cs
+++ src/Sfa.Eds.Das.Web/Models/SearchResultsItem.cs
@@ -5,6 +5,8 @@ namespace Sfa.Eds.Das.Web.Models
     public sealed class SearchResultsItem
     {
         public string Title { get; set; }
+        public int NotionalEndLevel { get; set; }
+        public string PdfUrl { get; set; }
         public float FundingCap { get; set; }
         public DateTime EffectiveEndDay { get; set; }
     }
