0e547e1266c099a2d0675290529baf87361fb484
Lee Wadhams <lee.wadhams@valtech.com>
2015-12-23 18:33:59 +0000

    Added a unit test (for test purpose)

--- src/Sfa.Eds.Das.Web.Tests/Controllers/HomeControllerTest.cs
+++ src/Sfa.Eds.Das.Web.Tests/Controllers/HomeControllerTest.cs
@@ -1,6 +1,8 @@
 ﻿using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Sfa.Eds.Das.Web.Controllers;
+using Sfa.Eds.Das.Web.Services;
+using Moq;
 
 namespace Sfa.Eds.Das.Web.Tests.Controllers
 {
@@ -15,6 +17,21 @@ namespace Sfa.Eds.Das.Web.Tests.Controllers
 
             // Act
             ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Search()
+        {
+            var mockSearchService = new Mock<ISearchForStandards>();
+
+            // Arrange
+            HomeController controller = new HomeController(mockSearchService.Object);
+
+            // Act
+            ViewResult result = controller.Search(new Models.SearchCriteria()) as ViewResult;
 
             // Assert
             Assert.IsNotNull(result);
