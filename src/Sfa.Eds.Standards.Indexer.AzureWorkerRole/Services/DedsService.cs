8f2582651c2f275a0950f216bff36172599b002a
mapaux <josevi.agullo@gmail.com>
2016-01-13 10:53:48 +0000

    Adding indexer worker role

---
+++ src/Sfa.Eds.Standards.Indexer.AzureWorkerRole/Services/DedsService.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.ServiceRuntime;
+using Sfa.Eds.Standards.Indexer.AzureWorkerRole.DedsService;
+
+namespace Sfa.Eds.Standards.Indexer.AzureWorkerRole.Services
+{
+    public class DedsService
+    {
+        private static string _searchEndpointConfiguration = CloudConfigurationManager.GetSetting("SearchEndpointConfigurationName");
+        const string datasetName = "LARS";
+
+        public static int GetNotationLevelFromLars(int standardId)
+        {
+            var b = CloudConfigurationManager.GetSetting("SearchEndpointConfigurationName");
+            var queryDescriptorStandard = GetQueryDescriptors(datasetName).Single(qd => qd.Name == "GetStandardCommonComponent");
+            var result = RunQuery(queryDescriptorStandard, standardId);
+
+            return result[1].Results.Length != 0 ? int.Parse(result[1].Results[0][5]) : 0;
+        }
+
+        /// <summary>
+        /// Query Descriptors contain parameter listings under the FilterDescriptors property
+        /// </summary>
+        /// <param name="dataSetName"></param>
+        /// <returns></returns>
+        private static QueryDescriptor[] GetQueryDescriptors(string dataSetName)
+        {
+            using (var client = new DedsSearchServiceClient(_searchEndpointConfiguration))
+            {
+                var dataSetVersionDescriptor = client.GetLatestPublishedDataSetVersion(dataSetName);
+
+                var queryDescriptors = client.DiscoverQueries(new DiscoverQueriesCriteria() { DataSetVersionId = dataSetVersionDescriptor.Id });
+                return queryDescriptors;
+            }
+        }
+
+        private static Dictionary<string, string> QueryFilterValuesFromConsole(QueryDescriptor queryDescriptor, int larsCode)
+        {
+            return queryDescriptor.FilterDescriptors.ToDictionary(filter => filter.FieldName, filter => larsCode.ToString());
+        }
+
+        private static QueryExecution GetQueryExecution(Dictionary<string, string> queryFilterValues, int? page, int? itemsPerPage)
+        {
+            var filterValues = new List<FilterValue>();
+
+            filterValues.AddRange(queryFilterValues.Select(queryFilterValue => new FilterValue
+            {
+                FieldName = queryFilterValue.Key,
+                FieldValue = queryFilterValue.Value
+            }));
+
+            var queryExecution = new QueryExecution
+            {
+                FilterValues = filterValues.Where(x => !string.IsNullOrEmpty(x.FieldValue)).ToArray(),
+                SortValues = new SortValue[0]
+            };
+
+            if (page.HasValue && itemsPerPage.HasValue)
+            {
+                queryExecution.PageNumber = page;
+                queryExecution.PageSize = itemsPerPage;
+            }
+
+            return queryExecution;
+        }
+
+        private static IList<QueryResults> ExecuteQuery(QueryDescriptor queryDescriptor, QueryExecution queryExecution)
+        {
+            using (var client = new DedsSearchServiceClient(_searchEndpointConfiguration))
+            {
+                return client.ExecuteQuery((Guid)queryDescriptor.Id, queryExecution);
+            }
+        }
+
+        private static IList<QueryResults> RunQuery(QueryDescriptor qds, int larsCode)
+        {
+            var queryFilterValues = QueryFilterValuesFromConsole(qds, larsCode);
+
+            var queryExecution = GetQueryExecution(queryFilterValues, null, null);
+
+            return ExecuteQuery(qds, queryExecution);
+        }
+    }
+}
---
+++ src/Sfa.Eds.Standards.Indexer.AzureWorkerRole/WorkerRole.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Diagnostics;
+using Microsoft.WindowsAzure.ServiceRuntime;
+using Microsoft.WindowsAzure.Storage;
+using Sfa.Eds.Standards.Indexer.AzureWorkerRole.Consumers;
+
+namespace Sfa.Eds.Standards.Indexer.AzureWorkerRole
+{
+    public class WorkerRole : RoleEntryPoint
+    {
+        private readonly CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+        private readonly ManualResetEvent runCompleteEvent = new ManualResetEvent(false);
+        private StandardControlQueueConsumer _standardControlQueueConsumer;
+
+        public override void Run()
+        {
+            Trace.TraceInformation("Sfa.Eds.Standards.Indexer.AzureWorkerRole is running");
+
+            _standardControlQueueConsumer = new StandardControlQueueConsumer();
+
+            while (true)
+            {
+                try
+                {
+                    var connectionString = "DefaultEndpointsProtocol=https;AccountName=indexerstorage;AccountKey=jVvVtb02mUNn/QiFJB71czOBNXqMYxj0UIpq3paGO+u3MiXzqxrbz7rO9RYASYkD/JXiRlWxn/s/o/lFYujkaA==";
+                    _standardControlQueueConsumer.CheckMessage(connectionString, "indexerqueue");
+                }
+                catch (Exception ex)
+                {
+                    //TODO: manage exceptions
+                }
+
+                Thread.Sleep(TimeSpan.FromMinutes(10));
+            }
+        }
+
+        public override bool OnStart()
+        {
+            // Set the maximum number of concurrent connections
+            ServicePointManager.DefaultConnectionLimit = 12;
+
+            // For information on handling configuration changes
+            // see the MSDN topic at http://go.microsoft.com/fwlink/?LinkId=166357.
+
+            bool result = base.OnStart();
+
+            Trace.TraceInformation("Sfa.Eds.Standards.Indexer.AzureWorkerRole has been started");
+
+            return result;
+        }
+
+        public override void OnStop()
+        {
+            Trace.TraceInformation("Sfa.Eds.Standards.Indexer.AzureWorkerRole is stopping");
+
+            this.cancellationTokenSource.Cancel();
+            this.runCompleteEvent.WaitOne();
+
+            base.OnStop();
+
+            Trace.TraceInformation("Sfa.Eds.Standards.Indexer.AzureWorkerRole has stopped");
+        }
+
+        private async Task RunAsync(CancellationToken cancellationToken)
+        {
+            // TODO: Replace the following with your own logic.
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                Trace.TraceInformation("Working");
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
