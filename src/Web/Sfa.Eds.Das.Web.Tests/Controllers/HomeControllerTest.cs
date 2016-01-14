98c8f84e9e18b15f00e008704e81bd9c0a3efe6b
Scott Cowan <info@scottcowan.org>
2016-01-14 11:31:10 +0000

    moving solutions into their own folders and updating stylecop in build

---
+++ src/Web/Sfa.Eds.Das.Web.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Web.Mvc;
+using Moq;
+using NUnit.Framework;
+using Sfa.Eds.Das.Web.Controllers;
+using Sfa.Eds.Das.Web.Services;
+
+namespace Sfa.Eds.Das.Web.Tests.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTest
+    {
+        [Test]
+        public void Index()
+        {
+            // Arrange
+            HomeController contr_oller = new HomeController(null);
+
+            // Act
+            ViewResult result = contr_oller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void Search()
+        {
+            var mockSearchService = new Mock<ISearchForStandards>();
+
+            // Arrange
+            HomeController controller = new HomeController(mockSearchService.Object);
+
+            // Act
+            ViewResult result = controller.Search(new Models.SearchCriteria()) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
