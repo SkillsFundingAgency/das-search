a85dc7df86521adc0d45b1784c9bd0a293867554
Froberg <christofer.froberg@gmail.com>
2016-01-27 08:36:19 +0000

    added separate log files for log config

--- src/Web/Sfa.Eds.Das.Web/Global.asax.cs
+++ src/Web/Sfa.Eds.Das.Web/Global.asax.cs
@@ -1,21 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using log4net;
 
 namespace Sfa.Eds.Das.Web
 {
+    
+
     public class MvcApplication : System.Web.HttpApplication
     {
+        private static readonly ILog Log = LogManager.GetLogger("HomeController");
+
         protected void Application_Start()
         {
+            Log.Info("Starting web applications...");
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            Log.Info("Web applications started...");
         }
     }
 }
