aa4b762bfa9d10e4f56fdefc27b2376fc8908da5
Froberg <christofer.froberg@gmail.com>
2016-01-29 16:10:51 +0000

    [Not sure what story in rally] - Integration UI css, images start page

--- src/Web/Sfa.Eds.Das.Web/Controllers/HomeController.cs
+++ src/Web/Sfa.Eds.Das.Web/Controllers/HomeController.cs
@@ -1,33 +1,28 @@
-﻿using System.Web.Mvc;
-using Sfa.Eds.Das.Web.Models;
-using Sfa.Eds.Das.Web.Services;
-
+﻿
 namespace Sfa.Eds.Das.Web.Controllers
 {
-    using System.Reflection;
-
-    using log4net;
+    using System.Web.Mvc;
+    using Models;
+    using Services;
 
     public class HomeController : Controller
     {
-        private static readonly ILog Log = LogManager.GetLogger("HomeController");
-        private readonly ISearchForStandards _searchService;
+        private readonly ISearchForStandards searchService;
 
         public HomeController(ISearchForStandards searchService)
         {
-            this._searchService = searchService;
+            this.searchService = searchService;
         }
 
         public ActionResult Index()
         {
-            Log.Warn("Hello from!");
             return View();
         }
 
         [HttpPost]
         public ActionResult Search(SearchCriteria criteria)
         {
-            var searchResults = _searchService.Search(criteria.Keywords);
+            var searchResults = this.searchService.Search(criteria.Keywords);
 
             return View(searchResults);
         }
