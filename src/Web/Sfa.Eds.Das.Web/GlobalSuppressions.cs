ec76b16beb2f6b96f98770cdceec60d91898a926
Lee Wadhams <lee.wadhams@valtech.com>
2016-01-30 17:37:45 +0000

    A couple of files missed from the last checkin

--- src/Web/Sfa.Eds.Das.Web/GlobalSuppressions.cs
+++ src/Web/Sfa.Eds.Das.Web/GlobalSuppressions.cs
@@ -3,6 +3,6 @@
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
 
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1027:Tabs must not be used", Justification = "<Pending>", Scope = "type", Target = "~T:Sfa.Eds.Das.Web.DependencyResolution.DefaultRegistry")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1027:Tabs must not be used", Justification = "<Pending>", Scope = "type", Target = "~T:Sfa.Eds.Das.Web.DependencyResolution.StructureMapDependencyScope")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1649:File name must match first type name", Justification = "<Pending>", Scope = "type", Target = "~T:Sfa.Eds.Das.Web.MvcApplication")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1027:Tabs must not be used", Justification = "File is from NuGet Package", Scope = "type", Target = "~T:Sfa.Eds.Das.Web.DependencyResolution.DefaultRegistry")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1027:Tabs must not be used", Justification = "File is from NuGet Package", Scope = "type", Target = "~T:Sfa.Eds.Das.Web.DependencyResolution.StructureMapDependencyScope")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1649:File name must match first type name", Justification = "Auto-generated", Scope = "type", Target = "~T:Sfa.Eds.Das.Web.MvcApplication")]
