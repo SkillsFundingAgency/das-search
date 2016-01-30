e47b6101e16b4f637f810e043d3959123580be19
Lee Wadhams <lee.wadhams@valtech.com>
2016-01-30 17:15:34 +0000

    Removed StyleCop msbuild task and replaced with Analyzer.

---
+++ src/Web/Sfa.Eds.Das.Web/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1027:Tabs must not be used", Justification = "<Pending>", Scope = "type", Target = "~T:Sfa.Eds.Das.Web.DependencyResolution.DefaultRegistry")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1027:Tabs must not be used", Justification = "<Pending>", Scope = "type", Target = "~T:Sfa.Eds.Das.Web.DependencyResolution.StructureMapDependencyScope")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1649:File name must match first type name", Justification = "<Pending>", Scope = "type", Target = "~T:Sfa.Eds.Das.Web.MvcApplication")]
