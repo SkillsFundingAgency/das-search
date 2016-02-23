6d5e2057f5503d63db0be0769f8b5237ed47c468
nasir.khan <nasir.khan@sfa.bis.gov.uk>
2016-02-23 10:56:46 +0000

    Added Acceptance test batch file.

---
+++ webtests/Sfa.Eds.Das.Web.AcceptanceTests/Acceptancetests.cmd
@@ -0,0 +1,18 @@
+@pushd %~dp0
+
+%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe "Sfa.Eds.Das.Web.AcceptanceTests.csproj"
+
+@if ERRORLEVEL 1 goto end
+
+@set filter=%1
+
+@cd ..\packages\SpecRun.Runner.*\tools
+
+@set profile=%1
+@if "%profile%" == "" set profile=Default
+
+SpecRun.exe run %profile%.srprofile "/baseFolder:%~dp0\bin\Debug" /log:specrun.log %2 %3 %4 %5
+
+:end
+
+@popd
