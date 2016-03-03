6f01324358a12db133c05ff697e7e6f93d370136
Lee Wadhams <lee.wadhams@valtech.com>
2016-03-03 10:56:40 +0000

    Added some logging to track error with postcode lookup in CI env

--- src/Web/Sfa.Eds.Das.Web/Controllers/ProviderController.cs
+++ src/Web/Sfa.Eds.Das.Web/Controllers/ProviderController.cs
@@ -10,14 +10,16 @@ namespace Sfa.Eds.Das.Web.Controllers
     using Sfa.Eds.Das.Web.Models;
     using Sfa.Eds.Das.Web.Services;
     using Sfa.Eds.Das.Web.ViewModels;
-
+    using log4net;
+    using System;
     public sealed class ProviderController : Controller
     {
+        private static readonly log4net.ILog Log = LogManager.GetLogger("MainLogger");
         private readonly IProviderSearchService _providerSearchService;
-        private readonly ILog _logger;
+        private readonly Core.Logging.ILog _logger;
         private readonly IMappingService _mappingService;
-
-        public ProviderController(IProviderSearchService providerSearchService, ILog logger, IMappingService mappingService)
+        
+        public ProviderController(IProviderSearchService providerSearchService, Core.Logging.ILog logger, IMappingService mappingService)
         {
             _providerSearchService = providerSearchService;
             _logger = logger;
@@ -27,16 +29,24 @@ namespace Sfa.Eds.Das.Web.Controllers
         [HttpGet]
         public async Task<ActionResult> SearchResults(ProviderSearchCriteria criteria)
         {
-            if (string.IsNullOrEmpty(criteria?.PostCode))
+            try
             {
-                return RedirectToAction("Detail", "Standard", new { id = criteria.StandardId, HasError = true });
+                if (string.IsNullOrEmpty(criteria?.PostCode))
+                {
+                    return RedirectToAction("Detail", "Standard", new { id = criteria.StandardId, HasError = true });
+                }
+
+                var searchResults = await _providerSearchService.SearchByPostCode(criteria.StandardId, criteria.PostCode);
+
+                var viewModel = _mappingService.Map<ProviderSearchResults, ProviderSearchResultViewModel>(searchResults);
+
+                return View(viewModel);
             }
-
-            var searchResults = await _providerSearchService.SearchByPostCode(criteria.StandardId, criteria.PostCode);
-
-            var viewModel = _mappingService.Map<ProviderSearchResults, ProviderSearchResultViewModel>(searchResults);
-
-            return View(viewModel);
+            catch (Exception ex)
+            {
+                Log.Error("PostCode lookup failed", ex);
+                throw;
+            }
         }
 
         [HttpGet]
