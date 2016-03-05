ed101efe9befdac35854a1a210f95209078fa632
Lee Wadhams <lee.wadhams@valtech.com>
2016-03-05 16:36:13 +0000

    Added ApplicationInsights logging capability

--- src/Web/Sfa.Eds.Das.Web/Global.asax.cs
+++ src/Web/Sfa.Eds.Das.Web/Global.asax.cs
@@ -6,7 +6,7 @@
     using System.Web.Routing;
     using System.Web.Http;
     using Core.Logging;
-
+    using System.Web.Configuration;
     public class MvcApplication : System.Web.HttpApplication
     {
         private ILog _logger;
@@ -17,6 +17,8 @@
 
             _logger.Info("Starting web applications...");
 
+            SetupApplicationInsights();
+
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
@@ -25,12 +27,16 @@
 
             _logger.Info("Web applications started...");
         }
-
         protected void Application_Error(object sender, EventArgs e)
         {
             Exception ex = Server.GetLastError().GetBaseException();
 
             _logger.Error(ex, "App_Error");
+        }
+
+        private void SetupApplicationInsights()
+        {
+            Microsoft.ApplicationInsights.Extensibility.TelemetryConfiguration.Active.InstrumentationKey = WebConfigurationManager.AppSettings["iKey"];
         }
     }
 }
