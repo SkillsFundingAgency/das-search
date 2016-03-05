bb1aacbf74f90e66616a42426c6167e447cb29b5
Lee Wadhams <lee.wadhams@valtech.com>
2016-03-05 17:25:51 +0000

    Added ApplicationsInsights.config file. Was being ignored.

--- src/Web/Sfa.Eds.Das.Web/DependencyResolution/SearchRegistry.cs
+++ src/Web/Sfa.Eds.Das.Web/DependencyResolution/SearchRegistry.cs
@@ -1,16 +1,5 @@
 namespace Sfa.Eds.Das.Web.DependencyResolution
 {
-    using ApplicationServices;
-    using Core.Logging;
-    using Infrastructure;
-    using Infrastructure.Logging;
-    using Infrastructure.PostCodeIo;
-
-    using Sfa.Das.ApplicationServices;
-    using Sfa.Eds.Das.Core.Configuration;
-    using Sfa.Eds.Das.Core.Domain.Services;
-    using Sfa.Eds.Das.Infrastructure.Configuration;
-    using Sfa.Eds.Das.Infrastructure.ElasticSearch;
     using Sfa.Eds.Das.Web.Services;
 
     using StructureMap.Configuration.DSL;
@@ -19,21 +8,7 @@ namespace Sfa.Eds.Das.Web.DependencyResolution
     {
         public SearchRegistry()
         {
-            For<IElasticsearchClientFactory>().Use<ElasticsearchClientFactory>();
-            For<IConfigurationSettings>().Use<ApplicationSettings>();
             For<IMappingService>().Use<MappingService>();
-            For<ILog>().Use(x => new NLogLogger(x.ParentType)).AlwaysUnique();
-
-            // New infrastructure
-            For<IElasticsearchClientFactory>().Use<ElasticsearchClientFactory>();
-            For<ILookupLocations>().Use<PostCodesIOLocator>();
-
-            For<IStandardSearchService>().Use<StandardSearchService>();
-            For<IStandardRepository>().Use<StandardRepository>();
-            For<ISearchProvider>().Use<ElasticsearchProvider>();
-
-            For<IProviderSearchService>().Use<ProviderSearchService>();
-            For<IRetryWebRequests>().Use<WebRequestRetryService>();
         }
     }
 }
