7fb186afb3be00609b606734d66dfcd63aea2a58
Lee Wadhams <lee.wadhams@valtech.com>
2016-03-05 18:19:45 +0000

    Add DI registry to ioc container.

--- src/Web/Sfa.Eds.Das.Web/DependencyResolution/IoC.cs
+++ src/Web/Sfa.Eds.Das.Web/DependencyResolution/IoC.cs
@@ -19,6 +19,7 @@
 namespace Sfa.Eds.Das.Web.DependencyResolution
 {
     using Infrastructure.DependencyResolution;
+    using Sfa.Das.ApplicationServices.DependencyResolution;
     using StructureMap;
 
     public static class IoC
@@ -29,6 +30,7 @@ namespace Sfa.Eds.Das.Web.DependencyResolution
                 c.AddRegistry<DefaultRegistry>();
                 c.AddRegistry<SearchRegistry>();
                 c.AddRegistry<InfrastructureRegistry>();
+                c.AddRegistry<ApplicationServicesRegistry>();
             });
         }
     }
