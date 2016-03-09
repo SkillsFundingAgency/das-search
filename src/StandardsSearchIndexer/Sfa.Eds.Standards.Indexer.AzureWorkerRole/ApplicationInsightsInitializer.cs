e8f02b9c9e563ae7d2fb5f68a2d7c6be2b862f01
froberg <christofer.froberg@gmail.com>
2016-03-09 14:40:17 +0000

    US000 added tag to Application insight in indexer, added layout for elasticsearch target

---
+++ src/StandardsSearchIndexer/Sfa.Eds.Standards.Indexer.AzureWorkerRole/ApplicationInsightsInitializer.cs
@@ -0,0 +1,12 @@
+namespace Sfa.Eds.Das.Indexer.AzureWorkerRole
+{
+    using Microsoft.ApplicationInsights.DataContracts;
+
+    public class ApplicationInsightsInitializer : Microsoft.ApplicationInsights.Extensibility.IContextInitializer
+    {
+        public void Initialize(TelemetryContext context)
+        {
+            context.Properties["application"] = "Sfa.Indexer";
+        }
+    }
+}
