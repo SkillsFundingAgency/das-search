02c223130fc45503ad92fe73bdc2e4f6fcbf8bc0
mapaux <josevi.agullo@gmail.com>
2016-03-11 13:32:22 +0000

    Added provider details page test class

---
+++ src/Web/Sfa.Eds.Das.Web.UnitTests/Views/Provider/ProviderDetailsPage.cs
@@ -0,0 +1,21 @@
+﻿namespace Sfa.Eds.Das.Web.UnitTests.Views.Provider
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using ExtensionHelpers;
+    using FluentAssertions;
+    using NUnit.Framework;
+    using RazorGenerator.Testing;
+
+    using Sfa.Das.ApplicationServices.Models;
+
+    using ViewModels;
+    using Web.Views.Provider;
+
+    [TestFixture]
+    public sealed class ProviderDetailsPage : ViewTestBase
+    {
+        
+    }
+}
