0256b6695fd954a8f61473fc1ecf8cf7a063203f
Lee Wadhams <lee.wadhams@valtech.com>
2016-04-01 12:18:31 +0100

    Removed unrequired conifg.

--- src/StandardsSearchIndexer/Sfa.Infrastructure/Settings/IInfrastructureSettings.cs
+++ src/StandardsSearchIndexer/Sfa.Infrastructure/Settings/IInfrastructureSettings.cs
@@ -5,15 +5,6 @@
 
     public interface IInfrastructureSettings
     {
-        string WorkerRolePauseTime { get; }
-        string ApprenticeshipIndexAlias { get; }
-        string StorageAccountName { get; }
-        string StorageAccountKey { get; }
-        string ConnectionString { get; }
-        string PauseTime { get; }
-        string StandardJsonContainer { get; }
-        string StandardPdfContainer { get; }
-        string StandardContentType { get; }
         string CourseDirectoryUri { get; }
 
         IEnumerable<Uri> ElasticServerUrls { get; }
--- src/StandardsSearchIndexer/Sfa.Infrastructure/Settings/InfrastructureSettings.cs
+++ src/StandardsSearchIndexer/Sfa.Infrastructure/Settings/InfrastructureSettings.cs
@@ -8,24 +8,6 @@
 
     public class InfrastructureSettings : BaseSettings, IInfrastructureSettings
     {
-        public string WorkerRolePauseTime => ConfigurationManager.AppSettings["WorkerRolePauseTime"];
-
-        public string ApprenticeshipIndexAlias => ConfigurationManager.AppSettings["ApprenticeshipIndexAlias"];
-
-        public string StorageAccountName => ConfigurationManager.AppSettings["StorageAccountName"];
-
-        public string StorageAccountKey => ConfigurationManager.AppSettings["StorageAccountKey"];
-
-        public string ConnectionString => $"DefaultEndpointsProtocol=https;AccountName={StorageAccountName};AccountKey={StorageAccountKey}";
-
-        public string PauseTime => ConfigurationManager.AppSettings["PauseTime"];
-
-        public string StandardJsonContainer => ConfigurationManager.AppSettings["Standard.JsonContainer"];
-
-        public string StandardPdfContainer => ConfigurationManager.AppSettings["Standard.PdfContainer"];
-
-        public string StandardContentType => ConfigurationManager.AppSettings["Standard.ContentType"];
-
         public string CourseDirectoryUri => ConfigurationManager.AppSettings["CourseDirectoryUri"];
 
         public IEnumerable<Uri> ElasticServerUrls => GetElasticIPs("ElasticServerUrls");
