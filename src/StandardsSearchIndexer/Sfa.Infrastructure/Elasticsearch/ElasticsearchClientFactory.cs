3f1e81c728acbf39a53cf1acfe744bc0412a73d3
froberg <christofer.froberg@gmail.com>
2016-04-05 14:10:01 +0100

    added dispose method to indexer elasticsearch client factory

--- src/StandardsSearchIndexer/Sfa.Infrastructure/Elasticsearch/ElasticsearchClientFactory.cs
+++ src/StandardsSearchIndexer/Sfa.Infrastructure/Elasticsearch/ElasticsearchClientFactory.cs
@@ -4,20 +4,25 @@ using Sfa.Infrastructure.Settings;
 
 namespace Sfa.Infrastructure.Elasticsearch
 {
-    public class ElasticsearchClientFactory : IElasticsearchClientFactory
+    using System;
+    public class ElasticsearchClientFactory : IElasticsearchClientFactory, IDisposable
     {
-        private readonly ConnectionSettings _connectionSettings;
+        private ConnectionSettings _connectionSettings;
 
         public ElasticsearchClientFactory(IInfrastructureSettings infrastructureSettings)
         {
-            var pool = new StaticConnectionPool(infrastructureSettings.ElasticServerUrls);
-            _connectionSettings = new ConnectionSettings(pool);
+            _connectionSettings = new ConnectionSettings(new StaticConnectionPool(infrastructureSettings.ElasticServerUrls));
         }
 
         public IElasticClient GetElasticClient()
         {
             var client = new ElasticClient(_connectionSettings);
             return client;
+        }
+
+        public void Dispose()
+        {
+            _connectionSettings = null;
         }
     }
 }
