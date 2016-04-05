fc4731a4daa37e7942fba85a52e0dd433a3f4f8d
mapaux <josevi.agullo@gmail.com>
2016-04-05 11:34:00 +0100

    Fixing error running tests

--- src/StandardsSearchIndexer/Sfa.Infrastructure/Elasticsearch/ElasticsearchMapper.cs
+++ src/StandardsSearchIndexer/Sfa.Infrastructure/Elasticsearch/ElasticsearchMapper.cs
@@ -81,14 +81,14 @@
                     return 2;
                 case 2:
                     return 3;
-                case 20:
-                    return 4;
-                case 21:
-                    return 5;
-                case 22:
-                    return 6;
-                case 23:
-                    return 7;
+                case 4:
+                    return 20;
+                case 5:
+                    return 21;
+                case 6:
+                    return 22;
+                case 7:
+                    return 23;
                 default:
                     return -1;
             }
@@ -102,14 +102,14 @@
                     return 2;
                 case 2:
                     return 3;
-                case 4:
-                    return 20;
-                case 5:
-                    return 21;
-                case 6:
-                    return 22;
-                case 7:
-                    return 23;
+                case 20:
+                    return 4;
+                case 21:
+                    return 5;
+                case 22:
+                    return 6;
+                case 23:
+                    return 7;
                 default:
                     return -1;
             }
