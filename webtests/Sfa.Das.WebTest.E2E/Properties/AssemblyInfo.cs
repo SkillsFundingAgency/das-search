743969213db4385fb58b3b684872e58dae8b4443
Scott Cowan <info@scottcowan.org>
2016-04-20 16:12:11 +0100

    US1565 setting local build version number to 0.0.0.0 for web test solution

--- webtests/Sfa.Das.WebTest.E2E/Properties/AssemblyInfo.cs
+++ webtests/Sfa.Das.WebTest.E2E/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Sfa.Das.Web.E2ETests")]
+[assembly: AssemblyTitle("Sfa.Das.WebTest.E2E")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Sfa.Das.Web.E2ETests")]
+[assembly: AssemblyProduct("Sfa.Das.WebTest.E2E")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -22,15 +22,15 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("749977ea-1b1c-4fcf-a8e8-22bf73445742")]
 
-// Version information for an assembly consists of the following four values:
+// BrowserVersion information for an assembly consists of the following four values:
 //
-//      Major Version
-//      Minor Version 
+//      Major BrowserVersion
+//      Minor BrowserVersion 
 //      Build Number
 //      Revision
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
--- webtests/Sfa.Das.WebTest.Pages/Properties/AssemblyInfo.cs
+++ webtests/Sfa.Das.WebTest.Pages/Properties/AssemblyInfo.cs
@@ -22,15 +22,15 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("b6d9b569-6b18-438e-b479-99eeeeb75b28")]
 
-// Version information for an assembly consists of the following four values:
+// BrowserVersion information for an assembly consists of the following four values:
 //
-//      Major Version
-//      Minor Version 
+//      Major BrowserVersion
+//      Minor BrowserVersion 
 //      Build Number
 //      Revision
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
