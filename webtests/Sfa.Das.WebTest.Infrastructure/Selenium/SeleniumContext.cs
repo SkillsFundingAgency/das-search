da8108129d720a35b52e0bd7d8e495c96962635f
Scott Cowan <info@scottcowan.org>
2016-04-20 16:25:20 +0100

    US1565 adding support for capabilities in browserstack to remote web driver

--- webtests/Sfa.Das.WebTest.Infrastructure/Selenium/SeleniumContext.cs
+++ webtests/Sfa.Das.WebTest.Infrastructure/Selenium/SeleniumContext.cs
@@ -3,6 +3,9 @@
     using System;
     using System.Configuration;
     using System.IO;
+    using System.Text.RegularExpressions;
+
+    using NUnit.Framework;
 
     using OpenQA.Selenium;
     using OpenQA.Selenium.Chrome;
@@ -40,20 +43,28 @@
         {
             if (!string.IsNullOrEmpty(_settings.RemoteUrl))
             {
-                if (!string.IsNullOrEmpty(_settings.BrowserStackUser))
-                {
-                    var capabilities = FindBrowserCapability();
-                    capabilities.SetCapability("os", _settings.OS);
-                    capabilities.SetCapability("os_version", _settings.OSVersion);
-                    capabilities.SetCapability("browser_version", _settings.Version);
-                    capabilities.SetCapability("browserstack.debug", "true");
-                    capabilities.SetCapability("browserstack.user", _settings.BrowserStackUser);
-                    capabilities.SetCapability("browserstack.key", _settings.BrowserStackKey);
-                    //capabilities.SetCapability("browserstack.selenium_version", "2.53.0");
+                var capabilities =
+                    FindBrowserCapability()
+                        .SafeSet("os", _settings.OS)
+                        .SafeSet("os_version", _settings.OSVersion)
+                        .SafeSet("browser_version", _settings.BrowserVersion)
+                        .SafeSet("browserstack.debug", "true")
+                        .SafeSet("browserstack.user", _settings.BrowserStackUser)
+                        .SafeSet("browserstack.key", _settings.BrowserStackKey)
+                        .SafeSet("project", _settings.Project)
+                        .SafeSet("device", _settings.Device)
+                        .SafeSet("resolution", _settings.Resolution);
 
-                    Console.WriteLine($"##### Driver: Browserstack - {_settings.OS} {_settings.OSVersion} - {_settings.Browser} {_settings.Version}");
-                    return new RemoteWebDriver(new Uri(_settings.RemoteUrl), capabilities);
+                if (_settings.AssemblyVersion != "0.0.0.0")
+                {
+                    capabilities.SetCapability("build", _settings.AssemblyVersion);
                 }
+                capabilities.SetCapability("name", GenerateTestName());
+                //capabilities.SetCapability("browserstack.selenium_version", "2.53.0");
+
+                Console.WriteLine($"##### Driver: Browserstack - {_settings.OS} {_settings.OSVersion} - {_settings.Device ?? _settings.Browser} {_settings.BrowserVersion}");
+                var remoteWebDriver = new RemoteWebDriver(new Uri(_settings.RemoteUrl), capabilities);
+                return remoteWebDriver;
             }
             else
             {
@@ -61,47 +72,21 @@
                 {
                     case "phantomjs":
                         return CreatePhantomJsDriver();
-                    case "chrome":
-                        return CreateChromeDriver();
-                    case "firefox":
-                        return CreateFireFoxDriver();
-                    case "remote":
-                        return CreateRemoteDriver();
                 }
-            }
-
-            if (host == "localhost")
-            {
-                if (testExecution == "headless") // headlessrun is performed on deployment server.
-                {
-                    return CreatePhantomJsDriver();
-                }
-
-                return CreateChromeDriver();
-            }
-
-            /*
-           Tests can be run on below devices available on browserstack tool. 
-            Android - Samsung Galaxy S5,Google Nexus 5,Samsung Galaxy Tab 4 10.1
-            IOS-iPhone 6S Plus,iPhone 6S,iPad Air,iPhone 5S
-            */
-
-            if (host == "browserstack")
-            {
-                return CreateBrowserStackDriver();
-            }
-
-            /* TestS can be run on below devices available in sauce labs tool
-            Andriod 4.4  : Google Nexus 7 HD Emulator, Samsung Galaxy S4 Emulator,Samsung Galaxy Tab 3 Emulator.
-            IOS 9.2 : iPad Air, iPhone 5, iPhone 6, iPhone 6 plus
-             */
-
-            if (host == "saucelabs")
-            {
-                return CreateSauceLabs();
             }
 
             return null;
+        }
+
+        private string GenerateTestName()
+        {
+            var arr = TestContext.CurrentContext.Test.Name.Replace(")", "").Replace(",null", "").Split('(');
+            return FeatureContext.Current.FeatureInfo.Title + " - " + SplitCamelCase(arr[0]) + " " + arr[1];
+        }
+
+        private string SplitCamelCase(string input)
+        {
+            return Regex.Replace(input, "(\\B[A-Z])", " $1");
         }
 
         private DesiredCapabilities FindBrowserCapability()
@@ -129,72 +114,14 @@
             throw new WebDriverException($"Browser Type '{_settings.Browser}' is not supported as a remote driver.");
         }
 
-        private IWebDriver CreateRemoteDriver()
-        {
-            throw new NotImplementedException();
-        }
-
-        private IWebDriver CreateFireFoxDriver()
-        {
-            var driver = new FirefoxDriver();
-            driver.Manage().Window.Maximize();
-            return driver;
-        }
-
-        private static IWebDriver CreateBrowserStackDriver()
-        {
-            DesiredCapabilities desiredCap = new DesiredCapabilities();
-            desiredCap.SetCapability("browserstack.key", browserStackKey);
-            desiredCap.SetCapability("browserstack.user", browserStacUser);
-            desiredCap.SetCapability("browserstack.debug", "true");
-
-            switch (mDevice)
-            {
-                case "Iphone":
-                    desiredCap.SetCapability("platform", "MAC"); // these values will be moved to app config.
-                    desiredCap.SetCapability("browserName", "iPhone");
-                    desiredCap.SetCapability("device", "iPhone 6S Plus");
-                    desiredCap.SetCapability("browserVersion", "8");
-                    break;
-
-                case "Samsung":
-                    desiredCap.SetCapability("platform", "Android"); // these values will be moved to app config.
-                    desiredCap.SetCapability("browserName", "Android");
-                    desiredCap.SetCapability("device", "Google Nexus 5");
-                    desiredCap.SetCapability("browserVersion", "5.0");
-                    break;
-            }
-
-            return new RemoteWebDriver(new Uri("http://hub.browserstack.com/wd/hub/"), desiredCap);
-        }
-
-        private static IWebDriver CreateSauceLabs()
-        {
-            DesiredCapabilities desiredCap = new DesiredCapabilities();
-            desiredCap.SetCapability("platform", "MAC");
-            desiredCap.SetCapability("browserName", "iPhone");
-            desiredCap.SetCapability("device", "iPhone 6 Plus");
-            desiredCap.SetCapability("browserVersion", "9.2");
-            desiredCap.SetCapability("username", saucelabsAccountName);
-            desiredCap.SetCapability("accessKey", saucelabsAccountKey);
-            //desiredCap.SetCapability("browserstack.debug", "true");
-            return new RemoteWebDriver(new Uri("http://ondemand.saucelabs.com:80/wd/hub/"), desiredCap, TimeSpan.FromSeconds(600));
-        }
-
-        private static IWebDriver CreateChromeDriver()
-        {
-            var dir = Directory.GetCurrentDirectory();
-            var driver = new ChromeDriver(Path.Combine(dir, @"..\\..\\Test\\Resources"));
-            driver.Manage().Window.Maximize();
-            return driver;
-        }
-
         private static IWebDriver CreatePhantomJsDriver()
         {
             var driverService = PhantomJSDriverService.CreateDefaultService();
             driverService.HideCommandPromptWindow = true;
             Console.WriteLine($"##### Driver: PhantomJs");
-            return new PhantomJSDriver(driverService);
+            var driver = new PhantomJSDriver(driverService);
+            driver.Manage().Window.Maximize();
+            return driver;
         }
 
         public void Dispose()
