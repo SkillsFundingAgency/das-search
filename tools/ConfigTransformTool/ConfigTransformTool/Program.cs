abe8e06756e2ef3b505c89b87c2cd5828393a264
Scott Cowan <info@scottcowan.org>
2016-04-21 16:16:20 +0100

    US1565 Adding script to copy and apply config transforms to the test dll

---
+++ tools/ConfigTransformTool/ConfigTransformTool/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ConfigTransformTool
+{
+    using System;
+
+    using Microsoft.Web.XmlTransform;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            TransformConfig(args[0], args[1], args[2]);
+        }
+
+        public static void TransformConfig(string configFileName, string transformFileName, string targetFileName)
+        {
+            var document = new XmlTransformableDocument();
+            document.PreserveWhitespace = true;
+            document.Load(configFileName);
+
+            var transformation = new XmlTransformation(transformFileName);
+            if (!transformation.Apply(document))
+            {
+                throw new Exception("Transformation Failed");
+            }
+            document.Save(targetFileName);
+        }
+    }
+}
