f79a4a1d95b759ea71844be460f044194795e45d
mapaux <josevi.agullo@gmail.com>
2016-04-21 10:41:14 +0100

    Fixing some SonarQube issues

--- src/Web/Sfa.Eds.Das.Web/hotfixTest.cs
+++ src/Web/Sfa.Eds.Das.Web/hotfixTest.cs
@@ -3,7 +3,7 @@
     /// <summary>
     /// IMPORTANT: This class is purely being used to test the release process so do not use as it will be deleted soon
     /// </summary>
-    public class hotfixTest
+    public class HotfixTest
     {
         // some new text
     }
