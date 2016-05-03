ba5c5b1d74ca04d03e8c58adb8de7ad39f2aebf8
Robin O'Connor <github@bytepointsoftware.com>
2016-05-03 13:52:33 +0100

    I've created a custom NLog layout with project that populated the assembly version to the log file and elasticsearch log entry as well.

---
+++ src/SearchIndexer/Sfa.Das.Sas.Nlog.CustomLayouts/AssemblyVersionNlogLayout.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Text;
+using NLog;
+using NLog.LayoutRenderers;
+
+namespace Sfa.Das.Sas.Nlog.CustomLayouts
+{
+    [LayoutRenderer("assembly-file-version")]
+    public class AssemblyVersionNlogLayout : LayoutRenderer
+    {
+        // As we only need this to be called once it is placed in a static variable
+        private static readonly string AssemblyVersion = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion;
+
+        protected override void Append(StringBuilder builder, LogEventInfo logEvent)
+        {
+            logEvent.Properties.Add("assembly-version", AssemblyVersion);
+            builder.Append(AssemblyVersion);
+        }
+    }
+}
