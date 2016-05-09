871cb0c78017a39f18945a3b3a479cf795d6b151
mapaux <josevi.agullo@gmail.com>
2016-05-09 13:37:04 +0100

    Added settings to tests

--- src/Web/Sfa.Das.Sas.ApplicationServices.UnitTests/StandardSearchServiceTest.cs
+++ src/Web/Sfa.Das.Sas.ApplicationServices.UnitTests/StandardSearchServiceTest.cs
@@ -1,5 +1,6 @@
 ﻿using Moq;
 using NUnit.Framework;
+using Sfa.Das.Sas.ApplicationServices.Settings;
 using Sfa.Das.Sas.Core.Logging;
 
 namespace Sfa.Das.Sas.ApplicationServices.UnitTests
@@ -9,19 +10,21 @@ namespace Sfa.Das.Sas.ApplicationServices.UnitTests
     {
         private Mock<ILog> _mockLogger;
         private Mock<ISearchProvider> _mockSearchProvider;
+        Mock<IPaginationSettings> _mockPaginationSettings;
 
         [SetUp]
         public void Setup()
         {
             _mockLogger = new Mock<ILog>();
             _mockSearchProvider = new Mock<ISearchProvider>();
+            _mockPaginationSettings = new Mock<IPaginationSettings>();
         }
 
         [Test]
         public void ShouldCallWithProvidedParameters()
         {
             _mockSearchProvider.Setup(m => m.SearchByKeyword(It.IsAny<string>(), It.IsAny<int>(), It.IsAny<int>()));
-            var standardSearchProvider = new ApprenticeshipSearchService(_mockSearchProvider.Object, _mockLogger.Object);
+            var standardSearchProvider = new ApprenticeshipSearchService(_mockSearchProvider.Object, _mockLogger.Object, _mockPaginationSettings.Object);
 
             standardSearchProvider.SearchByKeyword("test", 1, 2);
 
@@ -32,7 +35,8 @@ namespace Sfa.Das.Sas.ApplicationServices.UnitTests
         public void ShouldDefaultTakeTo10When0()
         {
             _mockSearchProvider.Setup(m => m.SearchByKeyword(It.IsAny<string>(), It.IsAny<int>(), It.IsAny<int>()));
-            var standardSearchProvider = new ApprenticeshipSearchService(_mockSearchProvider.Object, _mockLogger.Object);
+            _mockPaginationSettings.Setup(x => x.DefaultResultsAmount).Returns(10);
+            var standardSearchProvider = new ApprenticeshipSearchService(_mockSearchProvider.Object, _mockLogger.Object, _mockPaginationSettings.Object);
 
             standardSearchProvider.SearchByKeyword("test", 0, 0);
 
