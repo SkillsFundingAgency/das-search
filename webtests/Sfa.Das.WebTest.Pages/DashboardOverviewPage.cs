fb291bb63a8656915a62750b15ef59710c75001d
Robin O'Connor <github@bytepointsoftware.com>
2016-05-11 11:14:04 +0100

    US1666: I have added a domain to the cookie that is returned to clear the shortlist client cookie when the shortlist is empty. This will hopefully fix the bug of the remove standard link not working in non local environments.

---
+++ webtests/Sfa.Das.WebTest.Pages/DashboardOverviewPage.cs
@@ -0,0 +1,14 @@
+﻿namespace Sfa.Das.WebTest.Pages
+{
+    using OpenQA.Selenium;
+
+    using Sfa.Das.WebTest.Infrastructure;
+
+    [PageNavigation("/Dashboard/Overview")]
+    public class DashboardOverviewPage
+    {
+        public By StandardsShortlist => By.Id("standards-shortlist");
+
+        public By EmptyShortlistMessage => By.Id("empty-shortlist-message");
+    }
+}
