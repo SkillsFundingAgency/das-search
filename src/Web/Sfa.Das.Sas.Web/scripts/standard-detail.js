3a6f18b5e94b2b273103125b58b7d70e225a4cf2
Janusz 'Kali' Kaliszczak <janusz.kaliszczak@valtechcom.onmicrosoft.com>
2016-05-16 15:12:14 +0100

    reading appsettings

--- src/Web/Sfa.Das.Sas.Web/scripts/standard-detail.js
+++ src/Web/Sfa.Das.Sas.Web/scripts/standard-detail.js
@@ -6,25 +6,27 @@
     standard.Add = function(id) {
         var value = Cookies.get(standard.CookieName);
         if (!value || value.length === 0) {
-            Cookies.set(standard.CookieName, '' + id, { expires: 365, HttpOnly: true, path: '/' });
+            Cookies.set(standard.CookieName, '' + id, { expires: 365, domain: SearchAndShortlist.appsettings.cookieDomain, HttpOnly: SearchAndShortlist.appsettings.cookieSecure, path: '/' });
         } else {
             var array = value.split(',');
             var index = array.indexOf(id);
             if (index < 0) {
                 array.push(id);
             }
-            Cookies.set(standard.CookieName, array.join(','), { expires: 365, HttpOnly: true, path: '/' });
+            Cookies.set(standard.CookieName, array.join(','), { expires: 365, domain: SearchAndShortlist.appsettings.cookieDomain, HttpOnly: SearchAndShortlist.appsettings.cookieSecure, path: '/' });
         }
     };
 
     standard.Remove = function (id) {
         var value = Cookies.get(standard.CookieName);
-        var array = value.split(',');
-        var index = array.indexOf(id);
-        if (index >= 0) {
-            array.splice(index, 1);
+        if (value && value.length !== 0) {
+            var array = value.split(',');
+            var index = array.indexOf(id);
+            if (index >= 0) {
+                array.splice(index, 1);
+            }
+            Cookies.set(standard.CookieName, array.join(','), { expires: 365, domain: SearchAndShortlist.appsettings.cookieDomain, HttpOnly: SearchAndShortlist.appsettings.cookieSecure, path: '/' });
         }
-        Cookies.set(standard.CookieName, array.join(','), { expires: 365, HttpOnly: true, path: '/' });
     }
 
 
