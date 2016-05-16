7889ad2a061026421c44ad5c73ae4a2e9aa09bfd
Janusz 'Kali' Kaliszczak <janusz.kaliszczak@valtechcom.onmicrosoft.com>
2016-05-16 14:47:41 +0100

    Autosubmit after changing sorting method

--- src/Web/Sfa.Das.Sas.Web/scripts/ui.js
+++ src/Web/Sfa.Das.Sas.Web/scripts/ui.js
@@ -1,5 +1,19 @@
-﻿$(function () {
-    $('.filters .toggler').on('click', function () {
-        $(this).parents('.filters').toggleClass('unfolded');
-    })
-});
+﻿
+var ui = ui || {};
+(function () {
+
+    ui.init = function () {
+        $('.filters .toggler').on('click', function () {
+            $(this).parents('.filters').toggleClass('unfolded');
+        })
+
+        
+        $('html.js #search-results-order select').change(function (e) {
+            $('#search-results-order').submit();
+        })
+
+    };
+
+    ui.init();
+
+}());
