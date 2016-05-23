9df221e47173ebd8526c9f5c26521d3bc44ea3a4
Scott Cowan <info@scottcowan.org>
2016-05-23 10:11:16 +0100

    Adding a js test that references another file

---
+++ src/Web/Sfa.Das.Sas.Web/scripts/spec/sample.spec.js
@@ -0,0 +1,11 @@
+﻿/// <reference path="../app/sample.js"/>
+describe("A suite", function () {
+    it("contains spec with an expectation", function () {
+        expect(true).toBe(true);
+    });
+
+    it("refernces the sample js",
+        function() {
+            expect(MyTest).not.toBe(undefined);
+        });
+});
