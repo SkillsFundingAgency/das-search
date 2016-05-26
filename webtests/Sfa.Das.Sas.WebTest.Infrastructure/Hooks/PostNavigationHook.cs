6d6d21066f8c1f2a060ebc0601768b1e15ccba65
Scott Cowan <info@scottcowan.org>
2016-05-26 17:34:00 +0100

    Adding navigation post action to check javascript

---
+++ webtests/Sfa.Das.Sas.WebTest.Infrastructure/Hooks/PostNavigationHook.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sfa.Das.Sas.WebTest.Infrastructure.Hooks
+{
+    using System.Collections.ObjectModel;
+
+    using OpenQA.Selenium;
+
+    using Sfa.Das.Sas.WebTest.Infrastructure.Selenium;
+
+    using SpecBind.Actions;
+    using SpecBind.BrowserSupport;
+    using SpecBind.Pages;
+
+    public class TestPostNavigateHook : NavigationPostAction
+    {
+        private readonly IBrowser _browser;
+
+        public TestPostNavigateHook(IBrowser browser)
+        {
+            _browser = browser;
+        }
+
+        protected override void OnPageNavigate(IPage page, PageNavigationAction.PageAction actionType, IDictionary<string, string> pageArguments)
+        {
+            CheckForJavascriptErrors();
+        }
+
+        public void CheckForJavascriptErrors()
+        {
+            var errors = ((IJavaScriptExecutor)_browser.Driver()).ExecuteScript("return window.jsErrors") as ReadOnlyCollection<object> ?? new ReadOnlyCollection<object>(new List<object>());
+            if (errors.Any())
+            {
+                throw new ApplicationException(string.Join("\n", errors.Select(x => x.ToString())));
+            }
+        }
+    }
+}
