afaf4bc18719c7d8148d4c7bf259a152769773b7
Scott Cowan <info@scottcowan.org>
2016-05-27 14:53:11 +0100

    Adding powershell to modify browserfactory settings

---
+++ scripts/AppendBrowserFactory.ps1
@@ -0,0 +1,7 @@
+Param($config, $key, $value)
+
+$doc = New-Object System.Xml.XmlDocument
+$doc.Load($config)
+$node = $doc.SelectSingleNode('configuration/specbind/browserfactory/settings/add[@name="' + $key + '"]')
+$node.Attributes['value'].Value = $node.Attributes['value'].Value + ' ' + $value
+$doc.Save($config)
