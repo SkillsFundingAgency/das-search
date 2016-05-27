bfd4787b80758e31668b8dcfc20ec74ff78faa20
Scott Cowan <info@scottcowan.org>
2016-05-27 15:06:23 +0100

    fixing case sensetivity in powershell scripts

--- scripts/ModifyBrowserFactory.ps1
+++ scripts/ModifyBrowserFactory.ps1
@@ -2,6 +2,6 @@ Param($config, $key, $value)
 
 $doc = New-Object System.Xml.XmlDocument
 $doc.Load($config)
-$node = $doc.SelectSingleNode('configuration/specbind/browserfactory/settings/add[@name="' + $key + '"]')
+$node = $doc.SelectSingleNode('configuration/specBind/browserFactory/settings/add[@name="' + $key + '"]')
 $node.Attributes['value'].Value = $value
 $doc.Save($config)
