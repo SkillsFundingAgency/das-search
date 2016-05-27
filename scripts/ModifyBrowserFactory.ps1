949d029c7f7e31df34f78129132bbd70a8a3f7b8
Scott Cowan <info@scottcowan.org>
2016-05-27 15:07:56 +0100

    displaying node I'm trying to select

--- scripts/ModifyBrowserFactory.ps1
+++ scripts/ModifyBrowserFactory.ps1
@@ -3,5 +3,6 @@ Param($config, $key, $value)
 $doc = New-Object System.Xml.XmlDocument
 $doc.Load($config)
 $node = $doc.SelectSingleNode('configuration/specBind/browserFactory/settings/add[@name="' + $key + '"]')
+$node
 $node.Attributes['value'].Value = $value
 $doc.Save($config)
