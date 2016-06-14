351346a1c25d2afc03e2120e59fe04c505be92c5
Robin O'Connor <github@bytepointsoftware.com>
2016-06-14 14:30:35 +0100

    Added more unit tests to increase unit test coverage

---
+++ src/Web/Sfa.Das.Sas.Web.UnitTests/Infrastructure/Web/Factories/DashboardViewModelFactoryTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Sfa.Das.Sas.Web.Factories;
+using Sfa.Das.Sas.Web.ViewModels;
+using Assert = NUnit.Framework.Assert;
+
+namespace Sfa.Das.Sas.Web.UnitTests.Infrastructure.Web.Factories
+{
+    [TestFixture]
+    public class DashboardViewModelFactoryTest
+    {
+        private DashboardViewModelFactory _sut;
+
+        [SetUp]
+        public void Init()
+        {
+            _sut = new DashboardViewModelFactory();
+        }
+
+        [Test]
+        public void ShouldGetShortlistFrameworkViewModel()
+        {
+            // Assign
+            var standardViewModel = new ShortlistStandardViewModel();
+            var frameworkViewModel = new ShortlistFrameworkViewModel();
+            var standardViewModels = new List<ShortlistStandardViewModel> { standardViewModel };
+            var frameworkViewModels = new List<ShortlistFrameworkViewModel> { frameworkViewModel };
+
+            // Act
+            var viewModel = _sut.GetDashboardViewModel(standardViewModels, frameworkViewModels);
+
+            // Assert
+            Assert.Contains(standardViewModel, viewModel.Apprenticeships.ToList());
+            Assert.Contains(frameworkViewModel, viewModel.Apprenticeships.ToList());
+        }
+    }
+}
