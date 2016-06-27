5119b871d8132ba857ecf952747a90d411fabf06
leke <leke.ibiyode@sfa.bis.gov.uk>
2016-06-27 14:27:41 +0100

    Automated Regression Shortlisting

---
+++ webtests/Sfa.Das.Sas.WebTest.AcceptanceTests/Regression/CustomSteps/SpecialFunction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sfa.Das.Sas.WebTest.Pages;
+using OpenQA;
+
+namespace Sfa.Das.Sas.WebTest.AcceptanceTests.Regression.CustomSteps
+{
+   public  class SpecialFunction
+    {
+        public void OpenStart()
+        {
+            
+        }
+    }
+}
