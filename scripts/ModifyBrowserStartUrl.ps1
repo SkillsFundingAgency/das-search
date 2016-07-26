c59c26dc2c5c5c2ef229f2d51326b84ac9977df3
Lee Wadhams <lee.wadhams@valtech.com>
2016-07-26 10:08:50 +0100

    US00 Selecting starturl with attribute

--- scripts/ModifyBrowserStartUrl.ps1
+++ scripts/ModifyBrowserStartUrl.ps1
@@ -2,6 +2,6 @@ Param($config, $value)
 
 $doc = New-Object System.Xml.XmlDocument
 $doc.Load($config)
-$node = $doc.SelectSingleNode('configuration/specBind/application[@key="startUrl"]')
+$node = $doc.SelectSingleNode('configuration/specBind/application')
 $node.Attributes['startUrl'].Value = $value
 $doc.Save($config)
