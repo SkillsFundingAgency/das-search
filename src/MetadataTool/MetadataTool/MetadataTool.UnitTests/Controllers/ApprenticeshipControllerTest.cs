258f2dc32a78a03d58afca6c0fddef606de75c88
mapaux <josevi.agullo@gmail.com>
2016-08-10 15:48:46 +0100

    US2066 Added navigation from home page to standard and framework list pages

---
+++ src/MetadataTool/MetadataTool/MetadataTool.UnitTests/Controllers/ApprenticeshipControllerTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MetadataTool;
+using MetadataTool.Controllers;
+using MetadataTool.Web.Controllers;
+
+namespace MetadataTool.UnitTests.Controllers
+{
+    [TestClass]
+    public class ApprenticeshipControllerTest
+    {
+        [TestMethod]
+        public void Standards()
+        {
+            // Arrange
+            ApprenticeshipController controller = new ApprenticeshipController();
+
+            // Act
+            ViewResult result = controller.Standards() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Frameworks()
+        {
+            // Arrange
+            ApprenticeshipController controller = new ApprenticeshipController();
+
+            // Act
+            ViewResult result = controller.Frameworks() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ src/MetadataTool/MetadataTool/MetadataTool/Controllers/ApprenticeshipController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MetadataTool.Web.Controllers
+{
+    public class ApprenticeshipController : Controller
+    {
+        public ActionResult Standards()
+        {
+            return View();
+        }
+
+        public ActionResult Frameworks()
+        {
+            return View();
+        }
+    }
+}
