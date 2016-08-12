d9a692770609a449c3aa5807db2e82a77ae60878
mapaux <josevi.agullo@gmail.com>
2016-08-12 16:36:35 +0100

    US2066 Added API methods to retrieve Apprenticeship MetaData from repo

---
+++ src/Sfa.Das.Sas.MetadataTool/Sfa.Das.Sas.MetadataTool.Web/Controllers/API/FrameworkController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Sfa.Das.Sas.ApplicationServices.Helpers;
+using Sfa.Das.Sas.Core.Models;
+
+namespace Sfa.Das.Sas.MetadataTool.Web.Controllers.API
+{
+    public class FrameworkController : ApiController
+    {
+        private readonly IMetaDataHelper _metaDataHelper;
+
+        public FrameworkController(IMetaDataHelper metaDataHelper)
+        {
+            _metaDataHelper = metaDataHelper;
+        }
+
+        // GET: api/FrameworkApi
+        public IEnumerable<FrameworkMetaData> Get()
+        {
+            return _metaDataHelper.GetAllFrameworksMetaData();
+        }
+
+        // GET: api/FrameworkApi/5
+        public FrameworkMetaData Get(int id)
+        {
+            return _metaDataHelper.GetFrameworkMetaData(id);
+        }
+
+        // POST: api/FrameworkApi
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/FrameworkApi/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/FrameworkApi/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ src/Sfa.Das.Sas.MetadataTool/Sfa.Das.Sas.MetadataTool.Web/Controllers/API/StandardController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Sfa.Das.Sas.ApplicationServices.Helpers;
+using Sfa.Das.Sas.Core.Models;
+
+namespace Sfa.Das.Sas.MetadataTool.Web.Controllers.API
+{
+    public class StandardController : ApiController
+    {
+        private readonly IMetaDataHelper _metaDataHelper;
+
+        public StandardController(IMetaDataHelper metaDataHelper)
+        {
+            _metaDataHelper = metaDataHelper;
+        }
+
+        // GET: api/Standard
+        public IEnumerable<StandardMetaData> Get()
+        {
+            return _metaDataHelper.GetAllStandardsMetaData();
+        }
+
+        // GET: api/Standard/5
+        public StandardMetaData Get(int id)
+        {
+            return _metaDataHelper.GetStandardMetaData(id);
+        }
+        
+        // POST: api/Standard
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Standard/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Standard/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
