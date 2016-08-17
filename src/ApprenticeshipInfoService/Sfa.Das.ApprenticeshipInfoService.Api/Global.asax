9c7fdf1ceaa1ee29eeac1b18377440e12a0de4ad
Lee Wadhams <lee.wadhams@valtech.com>
2016-08-17 12:53:06 +0100

    US2069 Added Analyzers and renamed the projects

---
+++ src/ApprenticeshipInfoService/Sfa.Das.ApprenticeshipInfoService.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sfa.Das.ApprenticeshipInfoService.Api.WebApiApplication" Language="C#" %>
--- src/Sfa.Dad.Sas.ApprenticeshipInfoService/Sfa.Das.Sas.ApprenticeshipInfoService.Web/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace Sfa.Das.Sas.ApprenticeshipInfoService.Web.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
