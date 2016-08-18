ecfde6ebf15a26aaa5934ed9773d5be36d104b5d
mapaux <josevi.agullo@gmail.com>
2016-08-18 11:59:45 +0100

    US2069 Removed useless settings into ConfigurationSettings file

--- src/ApprenticeshipInfoService/Sfa.Das.ApprenticeshipInfoService.Core/Configuration/IConfigurationSettings.cs
+++ src/ApprenticeshipInfoService/Sfa.Das.ApprenticeshipInfoService.Core/Configuration/IConfigurationSettings.cs
@@ -9,32 +9,8 @@
 
         string ProviderIndexAlias { get; }
 
-        string BuildId { get; }
-
         IEnumerable<Uri> ElasticServerUrls { get; }
 
-        Uri SurveyUrl { get; }
-
-        string CookieInfoBannerCookieName { get; }
-
-        Uri PostcodeUrl { get; }
-
         string EnvironmentName { get; }
-
-        string ApplicationName { get; }
-
-        Uri SatisfactionSourceUrl { get; }
-
-        Uri CookieImprovementUrl { get; }
-
-        Uri CookieGoogleUrl { get; }
-
-        Uri CookieApplicationInsightsUrl { get; }
-
-        Uri CookieAboutUrl { get; }
-
-        Uri SurveyProviderUrl { get; }
-
-        Uri AchievementRateUrl { get; }
     }
 }
--- src/ApprenticeshipInfoService/Sfa.Das.ApprenticeshipInfoService.Infrastructure/Settings/ApplicationSettings.cs
+++ src/ApprenticeshipInfoService/Sfa.Das.ApprenticeshipInfoService.Infrastructure/Settings/ApplicationSettings.cs
@@ -13,33 +13,9 @@
 
         public string ProviderIndexAlias => ConfigurationManager.AppSettings["ProviderIndexAlias"];
 
-        public string BuildId => ConfigurationManager.AppSettings["BuildId"];
-
         public IEnumerable<Uri> ElasticServerUrls => GetElasticSearchIps();
 
-        public Uri SurveyUrl => new Uri(CloudConfigurationManager.GetSetting("SurveyUrl"));
-
-        public string CookieInfoBannerCookieName => ConfigurationManager.AppSettings["CookieInfoBannerCookieName"];
-
-        public Uri PostcodeUrl => new Uri(CloudConfigurationManager.GetSetting("PostcodeUrl"));
-
         public string EnvironmentName => ConfigurationManager.AppSettings["EnvironmentName"];
-
-        public string ApplicationName => ConfigurationManager.AppSettings["ApplicationName"];
-
-        public Uri SatisfactionSourceUrl => new Uri(CloudConfigurationManager.GetSetting("SatisfactionSourceUrl"));
-
-        public Uri AchievementRateUrl => new Uri(CloudConfigurationManager.GetSetting("AchievementRateUrl"));
-
-        public Uri CookieImprovementUrl => new Uri(CloudConfigurationManager.GetSetting("CookieImprovementUrl"));
-
-        public Uri CookieGoogleUrl => new Uri(CloudConfigurationManager.GetSetting("CookieGoogleUrl"));
-
-        public Uri CookieApplicationInsightsUrl => new Uri(CloudConfigurationManager.GetSetting("CookieApplicationInsightsUrl"));
-
-        public Uri CookieAboutUrl => new Uri(CloudConfigurationManager.GetSetting("CookieAboutUrl"));
-
-        public Uri SurveyProviderUrl => new Uri(CloudConfigurationManager.GetSetting("SurveyProviderUrl"));
 
         private IEnumerable<Uri> GetElasticSearchIps()
         {
