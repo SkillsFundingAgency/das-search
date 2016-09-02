c012ac7424dd647f067cf14e08b5e1179e2953ee
Christofer Froberg <Christofer_Froberg@valtech.com>
2016-09-02 16:09:20 +0100

    Added test module for frameworks

--- src/Sfa.Das.Sas.MetadataTool.Webtests/MetadataTool.Webtests/Program.fs
+++ src/Sfa.Das.Sas.MetadataTool.Webtests/MetadataTool.Webtests/Program.fs
@@ -11,19 +11,21 @@ let main argv =
 
   let drivers = sprintf "%s\%s" __SOURCE_DIRECTORY__ @"Tools\drivers\phantomjs\bin"
   canopy.configuration.phantomJSDir <- drivers
-  start phantomJS
+  //start phantomJS
+  start chrome
 
   printfn  "Drivers: %s" drivers
 
+  Frameworks.DisplayFrameworks testUrl
 
   "first test of meta data web sites" &&& fun _ -> 
     url testUrl
     ".jumbotron h1" == "Meta data tool"
 
-  "a test that should fail 2" &&& (fun _ ->
-    url testUrl
-    ".jumbotron h1" == "Meta data tool fail"
-  )
+//  "a test that should fail 2" &&& (fun _ ->
+//    url testUrl
+//    ".jumbotron h1" == "Meta data tool fail"
+//  )
 
   run()
 
---
+++ src/Sfa.Das.Sas.MetadataTool.Webtests/MetadataTool.Webtests/tests/Frameworks.fs
@@ -0,0 +1,40 @@
+﻿module Frameworks
+
+open canopy
+
+open System.Text.RegularExpressions
+
+let DisplayFrameworks baseUrl =
+  context "Displaying frameworks"
+  let u = sprintf "%s/%s" baseUrl "Apprenticeship/Frameworks"
+
+  "When listing all frameworks" &&& (fun _ -> 
+    url u
+
+    let title = read "h2"
+    let _,num = 
+        Regex.Match(title, "[0-9]+").Value
+        |> System.Int32.TryParse
+
+    count ".container p a" num
+  )
+
+  "When displaying a framework" &&& (fun _ -> 
+    url u
+    click "42924, Rail Services: Shunting "
+    click ".framework-details span a"
+    "#keyword-property input" << "Keyword 1"
+    //click "#keyword-property input"
+    press enter
+
+    "#keyword-property input" << "Keyword 2"
+    //click "#keyword-property input"
+    press enter
+
+    //count "#keyword-property .property-container .entry" 1
+
+    click (first "#keyword-property .entry .delete")
+
+    // count "#keyword-property .property-container .entry" 0
+    
+  )
