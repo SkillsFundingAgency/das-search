fb0c0cb49ed91562dddce89c38ebf09097df1172
Christofer Froberg <Christofer_Froberg@valtech.com>
2016-09-07 14:32:53 +0100

    US00 -> Added ToJson extension method instead of relying on MongoDB.Bson.ToJSON

---
+++ src/Sfa.Das.Sas.MetadataTool/Sfa.Das.Sas.Core/Extensions/ObjectExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Sfa.Das.Sas.Core.Extensions
+{
+    using System.Web.Script.Serialization;
+
+    public static class ObjectExtensions
+    {
+        public static string ToJson(this object obj)
+        {
+            var serializer = new JavaScriptSerializer();
+            return serializer.Serialize(obj);
+        }
+    }
+}
