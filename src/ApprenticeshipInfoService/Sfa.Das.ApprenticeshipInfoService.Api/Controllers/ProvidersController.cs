4ca4ac36598b7d3a6357e5981aec68274f56f276
mapaux <josevi.agullo@gmail.com>
2016-09-19 10:05:34 +0100

    FAT26 Saving some changes

--- src/ApprenticeshipInfoService/Sfa.Das.ApprenticeshipInfoService.Api/Controllers/ProvidersController.cs
+++ src/ApprenticeshipInfoService/Sfa.Das.ApprenticeshipInfoService.Api/Controllers/ProvidersController.cs
@@ -1,12 +1,26 @@
-﻿using System.Net;
-using Sfa.Das.ApprenticeshipInfoService.Core.Models;
-using Sfa.Das.ApprenticeshipInfoService.Core.Services;
-using Swashbuckle.Swagger.Annotations;
-
-namespace Sfa.Das.ApprenticeshipInfoService.Api.Controllers
+﻿namespace Sfa.Das.ApprenticeshipInfoService.Api.Controllers
 {
+    using System.Net;
+    using Sfa.Das.ApprenticeshipInfoService.Core.Models;
+    using Sfa.Das.ApprenticeshipInfoService.Core.Services;
+    using Swashbuckle.Swagger.Annotations;
+
     public class ProvidersController
     {
-        
+        private readonly IGetProviders _getProviders;
+
+        public ProvidersController(IGetProviders getProviders)
+        {
+            _getProviders = getProviders;
+        }
+
+        // GET api/values/5
+        [SwaggerOperation("GetById")]
+        [SwaggerResponse(HttpStatusCode.OK)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        public StandardProviderSearchResultsItem Get(int standardId, int orderId)
+        {
+            return new StandardProviderSearchResultsItem();
+        }
     }
 }
