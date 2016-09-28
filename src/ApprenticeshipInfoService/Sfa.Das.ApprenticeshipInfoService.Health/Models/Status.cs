3cdb9538a98cb05e16788663f285417663a8f9f9
Christofer Froberg <Christofer_Froberg@valtech.com>
2016-09-28 14:41:33 +0100

    FAT70 -> added String enum converter

--- src/ApprenticeshipInfoService/Sfa.Das.ApprenticeshipInfoService.Health/Models/Status.cs
+++ src/ApprenticeshipInfoService/Sfa.Das.ApprenticeshipInfoService.Health/Models/Status.cs
@@ -1,5 +1,9 @@
 namespace Sfa.Das.ApprenticeshipInfoService.Health.Models
 {
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum Status
     {
         Green = 0, Red = 1
