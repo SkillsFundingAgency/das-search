c333a7f0ed7740a011c9752aeee430897520c6ca
mapaux <josevi.agullo@gmail.com>
2016-10-10 11:37:40 +0100

    FAT79 Added Provider client

---
+++ src/ApprenticeshipInfoService/Sfa.Das.ApprenticeshipInfoService.Client/ProviderApiClient.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Sfa.Das.ApprenticeshipInfoService.Core.Models;
+
+namespace Sfa.Das.ApprenticeshipInfoService.Client
+{
+    public class ProviderApiClient : ApiClientBase, IProviderApiClient
+    {
+        public ProviderApiClient(string baseUri = null) : base(baseUri)
+        {
+        }
+
+        /// <summary>
+        /// Get a list of provider details
+        /// GET /providers/{provider-ukprn}
+        /// </summary>
+        /// <param name="providerUkprn">an integer for the composite id {frameworkId}{pathway}{progType}</param>
+        /// <returns>a framework details based on pathway and level</returns>
+        public IEnumerable<Provider> Get(int providerUkprn)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, $"/providers/{providerUkprn}");
+            request.Headers.Add("Accept", "application/json");
+
+            var response = _httpClient.SendAsync(request);
+
+            try
+            {
+                var result = response.Result;
+                if (result.StatusCode == HttpStatusCode.OK)
+                {
+                    return JsonConvert.DeserializeObject<IEnumerable<Provider>>(result.Content.ReadAsStringAsync().Result, _jsonSettings);
+                }
+
+                RaiseResponseError(request, result);
+            }
+            finally
+            {
+                Dispose(request, response);
+            }
+
+            return null;
+        }
+    }
+}
