a7d2ef45e314c599a354b6be94abbb342058ae2e
Scott Cowan <info@scottcowan.org>
2016-10-19 15:06:44 +0100

    cleaning up browser tests

--- webtests/Sfa.Das.Sas.WebTest.Infrastructure/Selenium/IBrowserStackApi.cs~HEAD
+++
@@ -1,9 +0,0 @@
-namespace Sfa.Das.WebTest.Infrastructure.Selenium
-{
-    using System;
-
-    public interface IBrowserStackApi
-    {
-        void FailTestSession(Exception testError);
-    }
-}
--- webtests/Sfa.Das.Sas.WebTest.Pages/StandardDetailsPage.cs
+++ webtests/Sfa.Das.Sas.WebTest.Pages/StandardDetailsPage.cs
@@ -11,8 +11,5 @@
     {
         [ElementLocator(CssSelector = "#bottom-actions .ui-find-training-providers")]
         public IWebElement SearchPageButton { get; set; }
-
-        [ElementLocator(CssSelector = ".shortlist-link")]
-        public IWebElement ShortlistLink { get; set; }
     }
 }
