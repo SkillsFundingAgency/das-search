7ab662aeb2c16c99e6e2c6626a0c65ec73dc9afc
sachin.patel <sachin.patel@valtech.co.uk>
2017-02-06 10:54:31 +0000

    CI-109 - FatLevyJourneyFeature toggle is based on date time configuration

--- src/Web/Sfa.Das.Sas.ApplicationServices/FeatureToggles/FatLevyJourneyFeature.cs
+++ src/Web/Sfa.Das.Sas.ApplicationServices/FeatureToggles/FatLevyJourneyFeature.cs
@@ -2,7 +2,7 @@
 
 namespace Sfa.Das.Sas.ApplicationServices.FeatureToggles
 {
-    public class FatLevyJourneyFeature : SimpleFeatureToggle
+    public class FatLevyJourneyFeature : EnabledOnOrAfterDateFeatureToggle
     {
     }
 }
