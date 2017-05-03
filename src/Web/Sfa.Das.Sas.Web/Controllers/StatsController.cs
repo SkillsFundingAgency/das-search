336ff255e602a67dc8127d18054d405eea47bd8e
Scott Cowan <info@scottcowan.org>
2017-05-03 16:32:18 +0100

    CI-262 removing stats page

--- src/Web/Sfa.Das.Sas.Web/Controllers/StatsController.cs
+++ src/Web/Sfa.Das.Sas.Web/Controllers/StatsController.cs
@@ -10,30 +10,30 @@ using SFA.DAS.NLog.Logger;
 
 namespace Sfa.Das.Sas.Web.Controllers
 {
-    [NoCache]
-    public sealed class StatsController : Controller
-    {
-        private readonly ILog _logger;
-        private readonly IMappingService _mappingService;
-        private readonly IMediator _mediator;
+    //[NoCache]
+    //public sealed class StatsController : Controller
+    //{
+    //    private readonly ILog _logger;
+    //    private readonly IMappingService _mappingService;
+    //    private readonly IMediator _mediator;
 
-        public StatsController(
-            ILog logger,
-            IMappingService mappingService,
-            IMediator mediator)
-        {
-            _logger = logger;
-            _mappingService = mappingService;
-            _mediator = mediator;
-        }
+    //    public StatsController(
+    //        ILog logger,
+    //        IMappingService mappingService,
+    //        IMediator mediator)
+    //    {
+    //        _logger = logger;
+    //        _mappingService = mappingService;
+    //        _mediator = mediator;
+    //    }
 
-        public ActionResult Stats()
-        {
-            var response = _mediator.Send(new StatsQuery());
+    //    public ActionResult Stats()
+    //    {
+    //        var response = _mediator.Send(new StatsQuery());
 
-            var viewModel = _mappingService.Map<StatsResponse, StatsViewModel>(response);
+    //        var viewModel = _mappingService.Map<StatsResponse, StatsViewModel>(response);
 
-            return View(viewModel);
-        }
-    }
+    //        return View(viewModel);
+    //    }
+    //}
 }
