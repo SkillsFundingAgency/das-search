2fa2fe6f9b8797ddea446c15823a2ef134b4913f
MarkFCain <mark.cain@alimar.co.uk>
2017-08-18 16:18:33 +0100

    CI-338 added a few real cases to stringurlhelper test

--- src/Web/Sfa.Das.Sas.Web.UnitTests/Infrastructure/Web/Helpers/StringUrlHelperTests.cs
+++ src/Web/Sfa.Das.Sas.Web.UnitTests/Infrastructure/Web/Helpers/StringUrlHelperTests.cs
@@ -7,8 +7,33 @@ namespace Sfa.Das.Sas.Web.UnitTests.Infrastructure.Web.Helpers
     [TestFixture]
     public class StringUrlHelperTests
     {
-        [TestCase("help", "help")]
-        [TestCase("help me", "help-me")]
+
+      /*  5 E LTD.
+    TRN(TRAIN) LTD.
+    ACTIVE LEARNING & DEVELOPMENT LIMITED
+    BARKING & DAGENHAM LONDON BOROUGH COUNCIL
+    AMERSHAM & WYCOMBE COLLEGE
+    ASSET TRAINING & CONSULTANCY LIMITED
+    ASTON RECRUITMENT & TRAINING LIMITED
+    BARNARDO'S
+    BARNET & SOUTHGATE COLLEGE
+    BASSETLAW TRAINING AGENCY(PROPERTIES) LIMITED
+    BERKSHIRE COLLEGE OF AGRICULTURE, THE(BCA)
+    BOURNEMOUTH AND POOLE COLLEGE, THE
+    "BMC (BROOKSBY MELTON COLLEGE)"
+    "C.M.S. VOCATIONAL TRAINING LIMITED"
+    DC TRAINING & DEVELOPMENT SERVICES LIMITED
+    EALING, HAMMERSMITH & WEST LONDON COLLEGE
+    "EAST MIDLANDS CHAMBER (DERBYSHIRE, NOTTINGHAMSHIRE, LEICESTERSHIRE)"
+    stringToProcess	"E.QUALITY TRAINING LIMITED"	string
+    "E.Q.V. (UK) LIMITED"
+    FASHION - ENTER LTD
+    MARITIME + ENGINEERING COLLEGE NORTH WEST
+    N & B TRAINING LTD
+    SOUTH & CITY COLLEGE BIRMINGHAM*/
+
+        [TestCase("5 E LTD.", "5-e-ltd")]
+        [TestCase("TRN(TRAIN) LTD.", "trn-train-ltd")]
 
         public void ShouldReturnStringModifiedForUrlUsage(string words, string modifiedwords)
         {
--- src/Web/Sfa.Das.Sas.Web/Helpers/StringUrlHelper.cs
+++ src/Web/Sfa.Das.Sas.Web/Helpers/StringUrlHelper.cs
@@ -11,7 +11,7 @@ namespace Sfa.Das.Sas.Web.Helpers
     {
      public string ModifyStringForUrlUsage(string stringToProcess)
         {
-            var firstpass = stringToProcess.ToLower().Replace("&", "and").Replace("+", "and").Replace(" ", "-");
+            var firstpass = stringToProcess.ToLower().Replace("&", "and").Replace("+", "and").Replace("("," "). Replace(")"," ").Replace(" ", "-");
             var secondpass = new StringBuilder();
             foreach (var c in firstpass)
             {
