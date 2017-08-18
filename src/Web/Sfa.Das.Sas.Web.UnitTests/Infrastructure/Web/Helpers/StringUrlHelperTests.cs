49350e81018d2e67c1316e8290072773c8bd853e
MarkFCain <mark.cain@alimar.co.uk>
2017-08-18 16:08:00 +0100

    CI-338 basic structure for stringurlhelper tests added

---
+++ src/Web/Sfa.Das.Sas.Web.UnitTests/Infrastructure/Web/Helpers/StringUrlHelperTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using Sfa.Das.Sas.Web.Helpers;
+using Assert = NUnit.Framework.Assert;
+
+namespace Sfa.Das.Sas.Web.UnitTests.Infrastructure.Web.Helpers
+{
+    [TestFixture]
+    public class StringUrlHelperTests
+    {
+        [TestCase("help", "help")]
+        [TestCase("help me", "help-me")]
+
+        public void ShouldReturnStringModifiedForUrlUsage(string words, string modifiedwords)
+        {
+            var actual = new StringUrlHelper().ModifyStringForUrlUsage(words);
+            Assert.AreEqual(actual, modifiedwords);
+
+        }
+    }
+}
