84b33d9e7369dc1cf00fd17032cffb3056f1b59d
MarkFCain <mark.cain@alimar.co.uk>
2017-08-23 15:13:39 +0100

    CI-338 added view tests

---
+++ src/Web/Sfa.Das.Sas.Web.UnitTests/Infrastructure/Web/Views/Provider/ProviderDetailsDetailsPageTests.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+using RazorGenerator.Testing;
+using Sfa.Das.Sas.Web.UnitTests.Infrastructure.Web.ExtensionHelpers;
+using Sfa.Das.Sas.Web.ViewModels;
+using Sfa.Das.Sas.Web.Views.Provider;
+
+namespace Sfa.Das.Sas.Web.UnitTests.Infrastructure.Web.Views.Provider
+{
+    [TestFixture]
+    public class ProviderDetailsDetailsPageTests : ViewTestBase
+    {
+        internal const string TradingNames = "item 1, Another Item, A different item";
+        internal const string Phone = "123-456";
+        internal const string ProviderName = "Joe The Plumbers";
+        internal const string Email = "test@test.com";
+        internal const long UkPrn = 2221221;
+        internal const string Website = "http://test.com";
+        internal const double NoSatisfactionScore = 0;
+        internal const string NoSatisfactionScoreMessage = "no data available";
+        internal const double SatisfactionScore = 15.9;
+        internal const string SatisfactionScoreMessage = "15.9%";
+
+        [Test]
+        public void ShouldShowFieldsAsExpected()
+        {
+            var providerDetails = new ProviderDetails();
+            var model = GetProvider();
+
+            var html = providerDetails.RenderAsHtml(model).ToAngleSharp();
+            GetPartial(html, "#provider-name").Should().Contain(ProviderName);
+            GetPartial(html, ".tag-national").Should().Contain("National");
+            GetPartial(html, ".data-list--provider").Should().Contain(TradingNames);
+            GetPartial(html, ".data-list--provider").Should().Contain(UkPrn.ToString());
+
+            GetPartial(html, ".apprenticeshipContactTitle").Should().Contain("Website");
+            GetPartial(html, ".apprenticeshipContact").Should().Contain(ProviderName + " website");
+            GetAttribute(html, ".apprenticeshipContact", "href").Should().Be(Website, "because http be added if missing");
+            GetPartial(html, ".phone-title").Should().Contain("Phone");
+            GetPartial(html, ".phone").Should().Contain(model.Phone);
+            GetPartial(html, ".email-title").Should().Contain("Email");
+            GetPartial(html, ".email").Should().Contain(model.Email);
+            GetPartial(html, "#employer-satisfaction").Should().Be(NoSatisfactionScoreMessage);
+            GetPartial(html, "#learner-satisfaction").Should().Contain(SatisfactionScoreMessage);
+            GetPartial(html, ".satisfaction-source").Should().Contain("Source:");
+        }
+
+        [Test]
+        public void ShouldShowNoNationalFieldIfNationalIsFalse()
+        {
+            var providerDetails = new ProviderDetails();
+            var model = GetProvider();
+            model.NationalProvider = false;
+            var html = providerDetails.RenderAsHtml(model).ToAngleSharp();
+            GetPartial(html, ".tag-national").Should().Be(string.Empty);
+        }
+
+        [Test]
+        public void ShouldShowEmployerSatisfactionIfNot0()
+        {
+            var providerDetails = new ProviderDetails();
+            var model = GetProvider();
+            model.EmployerSatisfaction = SatisfactionScore;
+            model.EmployerSatisfactionMessage = SatisfactionScoreMessage;
+            var html = providerDetails.RenderAsHtml(model).ToAngleSharp();
+            GetPartial(html, "#employer-satisfaction").Should().Contain(SatisfactionScoreMessage);
+        }
+
+        [Test]
+        public void ShouldShowLearnerSatisfactionIf0()
+        {
+            var providerDetails = new ProviderDetails();
+            var model = GetProvider();
+            model.LearnerSatisfaction = NoSatisfactionScore;
+            model.LearnerSatisfactionMessage = NoSatisfactionScoreMessage;
+            var html = providerDetails.RenderAsHtml(model).ToAngleSharp();
+            GetPartial(html, "#learner-satisfaction").Should().Be(NoSatisfactionScoreMessage);
+        }
+
+        [Test]
+        public void ShouldNotShowSourceIfNoSatisfactionScores()
+        {
+            var providerDetails = new ProviderDetails();
+            var model = GetProvider();
+            model.EmployerSatisfaction = NoSatisfactionScore;
+            model.EmployerSatisfactionMessage = NoSatisfactionScoreMessage;
+            model.LearnerSatisfaction = NoSatisfactionScore;
+            model.LearnerSatisfactionMessage = NoSatisfactionScoreMessage;
+
+            var html = providerDetails.RenderAsHtml(model).ToAngleSharp();
+            GetPartial(html, ".satisfaction-source").Should().Be(string.Empty);
+        }
+
+        private static ProviderDetailsViewModel GetProvider()
+        {
+            return new ProviderDetailsViewModel
+            {
+                TradingNames = TradingNames,
+                EmployerSatisfaction = NoSatisfactionScore,
+                EmployerSatisfactionMessage = NoSatisfactionScoreMessage,
+                LearnerSatisfaction = SatisfactionScore,
+                LearnerSatisfactionMessage = SatisfactionScoreMessage,
+                Email = Email,
+                IsEmployerProvider = true,
+                IsHigherEducationInstitute = true,
+                NationalProvider = true,
+                Phone = Phone,
+                Ukprn = UkPrn,
+                ProviderName = ProviderName,
+                Website = Website
+            };
+        }
+    }
+}
