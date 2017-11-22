9cd306364ae334e34fbb3cbafa52d2afe04f1c35
froberg <christofer.froberg@gmail.com>
2017-11-22 09:23:15 +0000

    Added take to search all query

--- src/Web/Sfa.Das.Sas.Infrastructure/ElasticSearch/ElasticsearchApprenticeshipSearchProvider.cs
+++ src/Web/Sfa.Das.Sas.Infrastructure/ElasticSearch/ElasticsearchApprenticeshipSearchProvider.cs
@@ -140,6 +140,7 @@ namespace Sfa.Das.Sas.Infrastructure.Elasticsearch
                 .Index(_applicationSettings.ApprenticeshipIndexAlias)
                 .AllTypes()
                 .Skip(skip)
+                .Take(take)
                 .Query(q => q
                     .QueryString(qs => qs
                         .Fields(fs => fs
