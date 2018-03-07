544b756dbad3a54cd1d16d3cd59ceeab7855c7c2
Ewan Noble <ewan.noble@education.gov.uk>
2018-03-07 16:04:48 +0000

    CloudConfigManager feature toggles and fix build

--- src/Web/Sfa.Das.Sas.ApplicationServices/FeatureToggles/RoatpProviderFlagsFeature.cs
+++ src/Web/Sfa.Das.Sas.ApplicationServices/FeatureToggles/RoatpProviderFlagsFeature.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FeatureToggle.Core;
 using FeatureToggle.Toggles;
 
 namespace Sfa.Das.Sas.ApplicationServices.FeatureToggles
 {
     public class RoatpProviderFlagsFeature : SimpleFeatureToggle
     {
+        public override IBooleanToggleValueProvider ToggleValueProvider { get => new CloudConfigToggleValueProvider(); set => base.ToggleValueProvider = value; }
     }
 }
--- src/Web/Sfa.Das.Sas.Infrastructure/FeatureToggles/Elk5Feature.cs
+++ src/Web/Sfa.Das.Sas.Infrastructure/FeatureToggles/Elk5Feature.cs
@@ -1,8 +1,10 @@
-﻿using FeatureToggle.Toggles;
+﻿using FeatureToggle.Core;
+using FeatureToggle.Toggles;
 
 namespace Sfa.Das.Sas.Infrastructure.FeatureToggles
 {
     public sealed class Elk5Feature : SimpleFeatureToggle
     {
+        public override IBooleanToggleValueProvider ToggleValueProvider { get => new CloudConfigToggleValueProvider(); set => base.ToggleValueProvider = value; }
     }
 }
