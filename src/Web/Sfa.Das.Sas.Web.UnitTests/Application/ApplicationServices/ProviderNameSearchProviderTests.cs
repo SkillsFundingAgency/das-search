3e8dce0895d3b9573616c62d70f65b29787308e1
markcain <mark.cain@digital.education.gov.uk>
2018-05-04 14:17:55 +0100

    CI-790 28 Feb B pagination orentation tests done

---
+++ src/Web/Sfa.Das.Sas.Web.UnitTests/Application/ApplicationServices/ProviderNameSearchProviderTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sfa.Das.Sas.Web.UnitTests.Application.ApplicationServices
+{
+    class ProviderNameSearchProviderTests
+    {
+    }
+}
+
---
+++ src/Web/Sfa.Das.Sas.Web.UnitTests/Application/Mapping/ProviderNameSearchMappingTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sfa.Das.Sas.Web.UnitTests.Application.Mapping
+{
+    class ProviderNameSearchMappingTests
+    {
+    }
+}
