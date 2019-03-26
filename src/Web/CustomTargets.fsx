7b8f7eaebff7d5fcbae9288ca8e4097d45de2e38
Matt Derry <matt.derry@digital.education.gov.uk>
2019-03-26 14:07:57 +0000

    Adds new unit tests project to FAKE build file

--- src/Web/CustomTargets.fsx
+++ src/Web/CustomTargets.fsx
@@ -7,4 +7,7 @@ Target "Dotnet Restore" (fun _ ->
     DotNetCli.Restore(fun p ->
         { p with
                 Project = ".\\Sfa.Das.Sas.Shared.Components.Web" })
+    DotNetCli.Restore(fun p ->
+        { p with
+                Project = ".\\Sfa.Das.Sas.Shared.Components.UnitTests" })
 )
