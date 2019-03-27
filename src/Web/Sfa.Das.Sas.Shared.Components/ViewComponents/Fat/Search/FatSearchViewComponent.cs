e1f5578920e00d0ae3c6aee7855c5c7d2b33c6f6
Matt Derry <matt.derry@digital.education.gov.uk>
2019-03-27 11:59:59 +0000

    Adds initial test view component from shared components library

--- src/Web/Sfa.Das.Sas.Shared.Components/ViewComponents/Fat/Search/FatSearchViewComponent.cs
+++ src/Web/Sfa.Das.Sas.Shared.Components/ViewComponents/Fat/Search/FatSearchViewComponent.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace Sfa.Das.Sas.Shared.Components.ViewComponents
 {
     public class FatSearchViewComponent : ViewComponent
     {
-        public IViewComponentResult Invoke(string searchRouteName, string keywords)
+        public async Task<IViewComponentResult> InvokeAsync(string searchRouteName, string keywords)
         {
             var model = new FatSearchViewModel
             {
