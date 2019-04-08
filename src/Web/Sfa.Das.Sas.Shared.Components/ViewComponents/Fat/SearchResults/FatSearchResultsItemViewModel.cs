6163062e65e7c5df8321cb3b6b0bafa0eae4796e
Matt Derry <matt.derry@digital.education.gov.uk>
2019-04-08 16:17:36 +0100

    Adds equivalent text to search results models

--- src/Web/Sfa.Das.Sas.Shared.Components/ViewComponents/Fat/SearchResults/FatSearchResultsItemViewModel.cs
+++ src/Web/Sfa.Das.Sas.Shared.Components/ViewComponents/Fat/SearchResults/FatSearchResultsItemViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Dynamic;
 using System.Text;
 using Sfa.Das.Sas.ApplicationServices.Models;
+using Sfa.Das.Sas.Resources;
 using Sfa.Das.Sas.Shared.Components.Domain.Interfaces;
 
 namespace Sfa.Das.Sas.Shared.Components.ViewComponents.Fat
@@ -14,7 +15,7 @@ namespace Sfa.Das.Sas.Shared.Components.ViewComponents.Fat
         public string Id { get; set; }
         public string Title { get; set; }
         public int Level { get; set; }
-        public string EquivalentText { get; set; }
+        public string EquivalentText => EquivalenceLevelService.GetApprenticeshipLevel(Level.ToString());
         public int Duration { get; set; }
         public DateTime? LastDateForNewStarts { get; set; }
         public ApprenticeshipType ApprenticeshipType { get; set; }
