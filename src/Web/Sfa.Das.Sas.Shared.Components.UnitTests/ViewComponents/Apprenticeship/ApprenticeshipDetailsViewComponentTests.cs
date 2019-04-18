593ea5ce25819bc2a732bc295ec0c670930733e7
Matt Derry <matt.derry@digital.education.gov.uk>
2019-04-18 11:33:30 +0100

    updates apprenticeshipDetails view component tests for frameworks

--- src/Web/Sfa.Das.Sas.Shared.Components.UnitTests/ViewComponents/Apprenticeship/ApprenticeshipDetailsViewComponentTests.cs
+++ src/Web/Sfa.Das.Sas.Shared.Components.UnitTests/ViewComponents/Apprenticeship/ApprenticeshipDetailsViewComponentTests.cs
@@ -6,6 +6,7 @@ using NUnit.Framework;
 using Sfa.Das.Sas.Shared.Components.ViewComponents;
 using System.Threading.Tasks;
 using Moq;
+using Sfa.Das.Sas.ApplicationServices.Models;
 using Sfa.Das.Sas.Shared.Components.Domain.Interfaces;
 using Sfa.Das.Sas.Shared.Components.Orchestrators;
 using Sfa.Das.Sas.Shared.Components.ViewComponents.ApprenticeshipDetails;
@@ -28,6 +29,11 @@ namespace Sfa.Das.Sas.Shared.Components.UnitTests.ViewComponents.Fat
 
             _apprenticeshipOrchestratorMock = new Mock<IApprenticeshipOrchestrator>(MockBehavior.Strict);
 
+            _apprenticeshipOrchestratorMock.Setup(s => s.GetFramework(It.Is<string>(g => g == "420-2-1"))).ReturnsAsync(new FrameworkDetailsViewModel(){Id = "420-2-1"});
+
+            _apprenticeshipOrchestratorMock.Setup(s => s.GetApprenticeshipType(It.Is<string>(g => g == "420-2-1"))).Returns(ApprenticeshipType.Framework);
+
+
             _sut = new ApprenticeshipDetailsViewComponent(_apprenticeshipOrchestratorMock.Object);
             _sut.ViewComponentContext = _viewComponentContext;
         }
@@ -42,6 +48,29 @@ namespace Sfa.Das.Sas.Shared.Components.UnitTests.ViewComponents.Fat
             result.Should().BeOfType<ViewViewComponentResult>();
 
             result.ViewName.Should().Be("Framework");
+        }
+
+        [Test]
+        public async Task When_Framework_Id_Is_Provided_Then_Return_FrameworkDetailsViewModel()
+        {
+            _apprenticeshipDetailQueryViewModel.Id = "420-2-1";
+
+            var result = await _sut.InvokeAsync(_apprenticeshipDetailQueryViewModel) as ViewViewComponentResult;
+
+            result.ViewData.Model.Should().BeOfType<FrameworkDetailsViewModel>();
+
+        }
+
+        [Test]
+        public async Task When_Framework_Id_Is_Provided_Then_Model_Is_Populated()
+        {
+            _apprenticeshipDetailQueryViewModel.Id = "420-2-1";
+
+            var result = await _sut.InvokeAsync(_apprenticeshipDetailQueryViewModel) as ViewViewComponentResult;
+
+            var model = result.ViewData.Model as FrameworkDetailsViewModel;
+
+            model.Id.Should().Be("420-2-1");
         }
     }
 }
