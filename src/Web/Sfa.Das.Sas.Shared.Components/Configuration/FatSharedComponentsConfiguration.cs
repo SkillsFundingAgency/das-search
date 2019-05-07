0f3c3be84ebabaacccf6af19bac945ccbacfc3d6
Lee Wadhams <lee.wadhams@valtech.co.uk>
2019-05-07 14:30:45 +0100

    Add save apprenticship component

--- src/Web/Sfa.Das.Sas.Shared.Components/Configuration/FatSharedComponentsConfiguration.cs
+++ src/Web/Sfa.Das.Sas.Shared.Components/Configuration/FatSharedComponentsConfiguration.cs
@@ -5,5 +5,6 @@ namespace Sfa.Das.Sas.Shared.Components.Configuration
     public class FatSharedComponentsConfiguration : IFatConfigurationSettings
     {
         public string FatApiBaseUrl { get; set; }
+        public string EmployerFavouritesUrl { get; set; }
     }
 }
---
+++ src/Web/Sfa.Das.Sas.Shared.Components/ViewComponents/SaveApprenticeship/SaveApprenticeshipViewComponent.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sfa.Das.Sas.Core.Configuration;
+using Sfa.Das.Sas.Shared.Components.ViewComponents.SaveApprenticeship;
+using System;
+using System.Web;
+
+namespace Sfa.Das.Sas.Shared.Components.ViewComponents.Basket.SaveApprenticeship
+{
+    public class SaveApprenticeshipViewComponent : ViewComponent
+    {
+        private readonly IFatConfigurationSettings _config;
+
+        public SaveApprenticeshipViewComponent(IFatConfigurationSettings config)
+        {
+            _config = config;
+        }
+
+        public IViewComponentResult Invoke(string apprenticeshipId)
+        {
+            var saveUrl = new Uri(new Uri(_config.EmployerFavouritesUrl), "/save-apprenticeship-favourites");
+            var builder = new UriBuilder(saveUrl);
+            var query = HttpUtility.ParseQueryString(builder.Query);
+
+            query["apprenticeshipId"] = apprenticeshipId;
+
+            builder.Query = query.ToString();
+
+            var model = new SaveApprenticeshipViewModel
+            {
+                SaveUrl = builder.Uri
+            };
+
+            return View("Default", model);
+        }
+    }
+}
