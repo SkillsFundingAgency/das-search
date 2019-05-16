f2c48acb4aa3747a4c91129f712ccba19f3caf64
Matt Derry <matt.derry@digital.education.gov.uk>
2019-05-16 11:21:22 +0100

    Adds training provider search view components and controller

---
+++ src/Web/Sfa.Das.Sas.Shared.Components/Controllers/TrainingProviderController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sfa.Das.Sas.ApplicationServices;
+using Sfa.Das.Sas.Shared.Components.ViewComponents.TrainingProvider.Search;
+using Sfa.Das.Sas.Shared.Components.ViewModels;
+
+namespace Sfa.Das.Sas.Shared.Components.Controllers
+{
+    public class TrainingProviderController : Controller
+    {
+        public IActionResult Search(TrainingProviderSearchViewModel model)
+        { 
+            return View("Components/TrainingProvider/SearchResults/Default", model);
+        }
+        
+    }
+}
