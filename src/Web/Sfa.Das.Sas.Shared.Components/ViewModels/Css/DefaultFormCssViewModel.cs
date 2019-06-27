14261dd108621c9206011b16dcb0e773477e7912
Matt Derry <matt.derry@digital.education.gov.uk>
2019-06-27 10:31:51 +0100

    Adds missing radio classes to models

--- src/Web/Sfa.Das.Sas.Shared.Components/ViewModels/Css/DefaultFormCssViewModel.cs
+++ src/Web/Sfa.Das.Sas.Shared.Components/ViewModels/Css/DefaultFormCssViewModel.cs
@@ -16,6 +16,8 @@ namespace Sfa.Das.Sas.Shared.Components.ViewModels.Css
 
         public string Radio => $"{_classPrefix}radios";
         public string RadioInput => $"{_classPrefix}radios__input";
+        public string RadioLabel => $"{_classPrefix}radios__label";
+        public string RadioItem => $"{_classPrefix}radios__item";
         public string RadioGroupInline => $"{Radio} radios--inline";
         public string Label => $"{_classPrefix}label";
     }
--- src/Web/Sfa.Das.Sas.Shared.Components/ViewModels/Css/Interfaces/IDefaultFormCssViewModel.cs
+++ src/Web/Sfa.Das.Sas.Shared.Components/ViewModels/Css/Interfaces/IDefaultFormCssViewModel.cs
@@ -6,6 +6,8 @@
         string FormGroup { get; }
         string Radio { get; }
         string RadioInput { get; }
+        string RadioLabel { get; }
+        string RadioItem { get; }
         string RadioGroupInline { get; }
         string Label { get; }
     }
