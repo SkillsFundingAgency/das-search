5cb9e27f2296d64ebf06ade2e0215feba8afef91
James King <hello@jamesmichaelking.com>
2019-07-17 15:28:53 +0100

    Style and copy changes to the autocomplete search box

--- src/Web/Sfa.Das.Sas.Web/scripts/app/ui.js
+++ src/Web/Sfa.Das.Sas.Web/scripts/app/ui.js
@@ -63,6 +63,7 @@ if ($container) {
         id: 'keywords',
         name: 'Keywords',
         displayMenu: 'overlay',
+        showNoOptionsFound: false,
         source: getSuggestions
     });
 }
