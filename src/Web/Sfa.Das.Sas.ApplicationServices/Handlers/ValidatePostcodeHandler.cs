ddeecb2c1f23ec95c26db2d13ba0fc22f4037bf9
Matt Derry <matt.derry@digital.education.gov.uk>
2019-10-31 11:54:49 +0000

    Adds remote validation of postcode for training provider search

---
+++ src/Web/Sfa.Das.Sas.ApplicationServices/Handlers/ValidatePostcodeHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using MediatR;
+using Sfa.Das.Sas.ApplicationServices.Queries;
+using Sfa.Das.Sas.ApplicationServices.Responses;
+using Sfa.Das.Sas.ApplicationServices.Services;
+
+namespace Sfa.Das.Sas.ApplicationServices.Handlers
+{
+    public class ValidatePostcodeHandler : IRequestHandler<ValidatePostcodeQuery, bool>
+    {
+        private readonly IPostcodeIoService _postcodeIoService;
+
+        public ValidatePostcodeHandler(IPostcodeIoService postcodeIoService)
+        {
+            _postcodeIoService = postcodeIoService;
+        }
+
+        public async Task<bool> Handle(ValidatePostcodeQuery request, CancellationToken cancellationToken)
+        {
+            if (String.IsNullOrWhiteSpace(request.Postcode))
+            {
+                return false;
+            }
+
+            var status = await _postcodeIoService.GetPostcodeStatus(request.Postcode);
+            switch (status)
+            {
+                case "England":
+                case "Wales":
+                case "Scotland":
+                case "Northern Ireland":
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
--- src/Web/Sfa.Das.Sas.Shared.Components/ViewComponents/TrainingProvider/Search/TrainingProviderSearchViewModel.cs
+++ src/Web/Sfa.Das.Sas.Shared.Components/ViewComponents/TrainingProvider/Search/TrainingProviderSearchViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
 using Sfa.Das.Sas.Shared.Components.ViewModels;
 
 namespace Sfa.Das.Sas.Shared.Components.ViewComponents.TrainingProvider.Search
@@ -12,6 +13,7 @@ namespace Sfa.Das.Sas.Shared.Components.ViewComponents.TrainingProvider.Search
         [Required]
         [DisplayName("Postcode")]
         [RegularExpression("\\b((?:(?:girGIR)|(?:[a-pr-uwyzA-PR-UWYZ])(?:(?:[0-9](?:[a-hjkpstuwA-HJKPSTUW]|[0-9])?)|(?:[a-hk-yA-HK-Y][0-9](?:[0-9]|[abehmnprv-yABEHMNPRV-Y])?)))) ?([0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2})\\b", ErrorMessage = "You must enter a valid postcode")]
+        [Remote(action: "ValidatePostcode",controller:"TrainingProvider",ErrorMessage = "You must enter a valid postcode")]
         public string Postcode { get; set; }
 
         [Required]
