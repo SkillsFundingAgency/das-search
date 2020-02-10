fab903518bfc4a49de439df2d4efc55aea7b049b
Paul Howes <paul.howes@cognetics.net>
2020-02-10 13:51:07 +0000

    CON-1308 Fixed unit test

--- src/Web/Sfa.Das.Sas.Web.UnitTests/Infrastructure/Web/Services/Mapping/ProviderSearchMapperTests.cs
+++ src/Web/Sfa.Das.Sas.Web.UnitTests/Infrastructure/Web/Services/Mapping/ProviderSearchMapperTests.cs
@@ -13,7 +13,7 @@ namespace Sfa.Das.Sas.Web.UnitTests.Infrastructure.Web.Services.Mapping
 
         [TestCase(ProviderSearchResponseCodes.LocationServiceUnavailable, "Sorry, postcode search not working, please try again later")]
         [TestCase(ProviderSearchResponseCodes.PostCodeTerminated, "This postcode is no longer valid; please search again")]
-        [TestCase(ProviderSearchResponseCodes.PostCodeInvalidFormat, "You must enter a full and valid postcode")]
+        [TestCase(ProviderSearchResponseCodes.PostCodeInvalidFormat, "Enter the postcode of your apprentice’s workplace")]
         [TestCase(ProviderSearchResponseCodes.WalesPostcode, "The postcode entered is not in England. Information about apprenticeships in <a href=\"https://businesswales.gov.wales/skillsgateway/apprenticeships\">Wales</a>")]
         [TestCase(ProviderSearchResponseCodes.NorthernIrelandPostcode, "The postcode entered is not in England. Information about apprenticeships in <a href=\"https://www.nibusinessinfo.co.uk/content/apprenticeships-employers\">Northern Ireland</a>")]
         [TestCase(ProviderSearchResponseCodes.ScotlandPostcode, "The postcode entered is not in England. Information about apprenticeships in <a href=\"https://www.apprenticeships.scot/\">Scotland</a>")]
